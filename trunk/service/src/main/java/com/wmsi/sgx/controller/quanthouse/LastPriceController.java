42bffeeb17ace15b4abc6f1963b71ee7b019eb2f
justin_lee <justin_lee@c31ae540-8fef-4b06-900f-01f520cbaedc>
2014-02-21 20:35:40 +0000

    Moved to annotation based config,added global exception handler fallback and local exception handler to price controller.

--- trunk/service/src/main/java/com/wmsi/sgx/controller/quanthouse/LastPriceController.java
+++ trunk/service/src/main/java/com/wmsi/sgx/controller/quanthouse/LastPriceController.java
@@ -1,30 +1,44 @@
 package com.wmsi.sgx.controller.quanthouse;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.wmsi.sgx.model.Price;
+import com.wmsi.sgx.service.quanthouse.InvalidInstrumentException;
 import com.wmsi.sgx.service.quanthouse.QuanthouseService;
 import com.wmsi.sgx.service.quanthouse.QuanthouseServiceException;
 
 @Controller
-@RequestMapping(produces="application/json")
+@RequestMapping(value="/{id}", produces="application/json")
 public class LastPriceController {
 
 	@Autowired
 	private QuanthouseService service;
 	
-	@RequestMapping("lastPrice")
-	public @ResponseBody Double lastPrice() throws QuanthouseServiceException{
-		return service.getLastPrice("XSES", "C6L");
-	}
-	
 	@RequestMapping("price")
-	public @ResponseBody Price getPrice() throws QuanthouseServiceException{
-		return service.getPrice("XSES", "C6L");
+	public @ResponseBody Price getPrice(@PathVariable String id) throws QuanthouseServiceException{
+		return service.getPrice("XSES", id);
 	}
 
+	@ExceptionHandler(InvalidInstrumentException.class )
+	public @ResponseBody Object handleInvalidInstrumentException(InvalidInstrumentException ex) {
+		Map<String, String> err = new HashMap<String, String>();
+		err.put("error", "invalid id");		
+	    return err;
+	}
+
+	@ExceptionHandler(Exception.class )
+	public @ResponseBody Object handleException(Exception ex) {
+		Map<String, String> err = new HashMap<String, String>();
+		err.put("error", "invalid request");
+	    return err;
+	}
 }
 
