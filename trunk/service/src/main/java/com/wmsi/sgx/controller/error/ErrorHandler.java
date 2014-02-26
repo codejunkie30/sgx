22f9431a1b7aade58bf1b1d1c0f3fdb941ca5826
justin_lee <justin_lee@c31ae540-8fef-4b06-900f-01f520cbaedc>
2014-02-26 17:16:29 +0000

    Refactoring exception handling to common class

---
+++ trunk/service/src/main/java/com/wmsi/sgx/controller/error/ErrorHandler.java
@@ -0,0 +1,32 @@
+package com.wmsi.sgx.controller.error;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import com.wmsi.sgx.service.quanthouse.InvalidInstrumentException;
+
+@ControllerAdvice
+public class ErrorHandler{
+	
+	private Logger log = LoggerFactory.getLogger(ErrorHandler.class);
+	
+	@ExceptionHandler(InvalidInstrumentException.class )
+	@ResponseStatus(HttpStatus.BAD_REQUEST)	
+	public @ResponseBody ErrorResponse handleInvalidInstrumentException(InvalidInstrumentException e) {
+		log.error("Invalid ID requested");
+		return new ErrorResponse(new ErrorMessage("Invalid ID", 4001));
+	}
+
+	@ExceptionHandler(Throwable.class)
+	@ResponseStatus(HttpStatus.BAD_REQUEST)	
+	public @ResponseBody ErrorResponse handleException(Throwable e) {
+		log.error("Caught unkown exception", e);
+		return new ErrorResponse(new ErrorMessage("Bad Request", 4001));	    
+	}	
+
+}
--- trunk/service/src/main/java/com/wmsi/sgx/controller/quanthouse/PriceController.java
+++ trunk/service/src/main/java/com/wmsi/sgx/controller/quanthouse/PriceController.java
@@ -3,19 +3,12 @@ package com.wmsi.sgx.controller.quanthouse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.wmsi.sgx.controller.error.ErrorMessage;
-import com.wmsi.sgx.controller.error.ErrorResponse;
 import com.wmsi.sgx.model.Price;
-import com.wmsi.sgx.service.quanthouse.InvalidInstrumentException;
 import com.wmsi.sgx.service.quanthouse.QuanthouseService;
 import com.wmsi.sgx.service.quanthouse.QuanthouseServiceException;
 
@@ -33,20 +26,6 @@ public class PriceController {
 	@RequestMapping(value = "price", method = RequestMethod.POST )
 	public Price getPrice(@PathVariable String id) throws QuanthouseServiceException{
 		return service.getPrice(market, id);
-	}
-
-	@ExceptionHandler(InvalidInstrumentException.class )
-	@ResponseStatus(HttpStatus.BAD_REQUEST)	
-	public @ResponseBody ErrorResponse handleInvalidInstrumentException(InvalidInstrumentException e) {
-		log.error("Invalid ID passed to PriceController");
-		return new ErrorResponse(new ErrorMessage("Invalid ID", 4001));
-	}
-
-	@ExceptionHandler(Exception.class)
-	@ResponseStatus(HttpStatus.BAD_REQUEST)	
-	public ErrorResponse handleException(Exception e) {
-		log.error("Caught exception in PriceController", e);
-		return new ErrorResponse(new ErrorMessage("Bad Request", 4001));	    
 	}
 }
 
