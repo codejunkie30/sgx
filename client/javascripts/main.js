9f5b6f8299c6411e9a5ba70b17a7877e6f89d0ed
mheinerWMSI <mheinerWMSI@c31ae540-8fef-4b06-900f-01f520cbaedc>
2014-02-27 04:56:14 +0000

    Snapshot update for Mike. Still migrating away from RequireJS AMD setup.

--- client/javascripts/main.js
+++ client/javascripts/main.js
@@ -1,42 +1,49 @@
 (function() {
 
-  require({
-    urlArgs: "b=" + ((new Date()).getTime()),
-    paths: {
-      jquery: 'vendor/jquery.ui/jquery-1.10.2',
+    require({
+        urlArgs: "b=" + ((new Date()).getTime()),
+        paths: {
+            jquery: 'vendor/jquery-modern/jquery',
+            jqueryLegacy: 'vendor/jquery-legacy/jquery',
+            jqueryModern: 'vendor/jquery-modern/jquery',
+            jquicore: 'vendor/jquery.ui/ui/jquery.ui.core',
+            jquiwidget: 'vendor/jquery.ui/ui/jquery.ui.widget',
+            jquimouse: 'vendor/jquery.ui/ui/jquery.ui.mouse',
+            accordion: 'vendor/jquery.ui/ui/jquery.ui.accordion',
+            slider: 'vendor/jquery.ui/ui/jquery.ui.slider',
+            tabs: 'vendor/jquery.ui/ui/jquery.ui.tabs',
+            highcharts: 'vendor/highcharts/index',
+            highstocks: 'vendor/highstock'
+        },
+        shim: {
+            jquery: {
+                exports: '$'
+            },
+            jquicore: {
+                deps: ['jquery']
+            },
+            jquiwidget: {
+                deps: ['jquery', 'jquicore']
+            },
+            jquimouse: {
+                deps: ['jquery', 'jquicore', 'jquiwidget']
+            },
+            accordion: {
+                deps: ['jquery', 'jquicore', 'jquiwidget', 'jquimouse']
+            },
+            slider: {
+                deps: ['jquery', 'jquicore', 'jquiwidget', 'jquimouse']
+            },
+            tabs: {
+                deps: ['jquery', 'jquicore', 'jquiwidget']
+            },
+            highcharts: {
+                exports: "highcharts",
+                deps: ['jquery']
+            }
+        }
+    }, ['app/SGX-main', 'app/SGX-stocks'], function($) {
 
-      jquicore: 'vendor/jquery.ui/ui/jquery.ui.core',
-      jquiwidget: 'vendor/jquery.ui/ui/jquery.ui.widget',
-      jquimouse: 'vendor/jquery.ui/ui/jquery.ui.mouse',
-      accordion: 'vendor/jquery.ui/ui/jquery.ui.accordion',
-      slider: 'vendor/jquery.ui/ui/jquery.ui.slider',
-      tabs: 'vendor/jquery.ui/ui/jquery.ui.tabs'
-    },
-    shim: {
-      jquery: {
-        exports: '$'
-      },
-      jquicore: {
-        deps: ['jquery']
-      },
-      jquiwidget: {
-        deps: ['jquery', 'jquicore']
-      },
-      jquimouse: {
-        deps: ['jquery', 'jquicore', 'jquiwidget']
-      },
-      accordion: {
-        deps: ['jquery', 'jquicore', 'jquiwidget', 'jquimouse']
-      },
-      slider: {
-        deps: ['jquery', 'jquicore', 'jquiwidget', 'jquimouse']
-      },
-      tabs: {
-        deps: ['jquery', 'jquicore', 'jquiwidget']
-      }
-    }
-  }, ['app/SGX-main'], function($) {
-    
-  });
+    });
 
 }).call(this);
