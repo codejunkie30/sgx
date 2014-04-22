ca66c724d1f2063222b07ed4349048b3f9083c33
michael_parcewski <michael_parcewski@c31ae540-8fef-4b06-900f-01f520cbaedc>
2014-04-22 15:00:27 +0000

    added experimental align plugin for highcharts

--- trunk/client/javascripts/app/SGX-base.js
+++ trunk/client/javascripts/app/SGX-base.js
@@ -235,7 +235,7 @@ define(['jquery', 'underscore', 'jquicore', 'jquiwidget', 'jquimouse', 'jquidate
                 
                 drawCriteria: function(data) {
                 	
-                	var runsearch = false;
+                	var runsearch = true;
                 	
                 	if (data.distributions.length > 1) {
                     	data.distributions.sort(function(a, b) {
@@ -2003,7 +2003,8 @@ define(['jquery', 'underscore', 'jquicore', 'jquiwidget', 'jquimouse', 'jquidate
             		var chart = $('#large-bar-chart').highcharts({
             			
             			chart: {
-            				type: 'column'
+            				type: 'column',
+            				alignThresholds: true
             			},
             			legend: {
             				enabled: false
@@ -2214,6 +2215,65 @@ define(['jquery', 'underscore', 'jquicore', 'jquiwidget', 'jquimouse', 'jquidate
 
             
     };
+
+    
+    /**
+     * Experimental Highcharts plugin to implement chart.alignThreshold option.
+     * Author: Torstein Hønsi
+     * Last revision: 2013-12-02
+     */
+    (function (H) {
+        var each = H.each;
+        H.wrap(H.Chart.prototype, 'adjustTickAmounts', function (proceed) {
+            var ticksBelowThreshold = 0,
+                ticksAboveThreshold = 0;
+            if (this.options.chart.alignThresholds) {
+                each(this.yAxis, function (axis) {
+                    var threshold = axis.series[0] && axis.series[0].options.threshold || 0,
+                        index = axis.tickPositions && $.inArray(threshold, axis.tickPositions);
+
+                    if (index !== undefined && index !== -1) {
+                        axis.ticksBelowThreshold = index;
+                        axis.ticksAboveThreshold = axis.tickPositions.length - index;
+                        ticksBelowThreshold = Math.max(ticksBelowThreshold, index);
+                        ticksAboveThreshold = Math.max(ticksAboveThreshold, axis.ticksAboveThreshold);
+                    }
+                });
+
+                each(this.yAxis, function (axis) {
+                    
+                    var tickPositions = axis.tickPositions;
+
+                    if (tickPositions) {
+
+                        if (axis.ticksAboveThreshold < ticksAboveThreshold) {
+                            while (axis.ticksAboveThreshold < ticksAboveThreshold) {
+                                tickPositions.push(
+                                    tickPositions[tickPositions.length - 1] + axis.tickInterval
+                                );
+                                axis.ticksAboveThreshold++;
+                            }
+                        }
+
+                        if (axis.ticksBelowThreshold < ticksBelowThreshold) {
+                            while (axis.ticksBelowThreshold < ticksBelowThreshold) {
+                                tickPositions.unshift(
+                                    tickPositions[0] - axis.tickInterval
+                                );
+                                axis.ticksBelowThreshold++;
+                            }
+
+                        }
+                        axis.min = tickPositions[0];
+                        axis.max = tickPositions[tickPositions.length - 1];
+                    }
+                });
+            } else {
+                proceed.call(this);
+            }
+
+        })
+    }(Highcharts));    
     
     SGX.init();    
 
