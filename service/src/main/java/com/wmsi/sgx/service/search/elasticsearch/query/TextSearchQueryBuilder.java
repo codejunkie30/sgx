c217d70336e72c1991b776ec625ba74ebdd1f6ae
justin_lee <justin_lee@c31ae540-8fef-4b06-900f-01f520cbaedc>
2014-04-23 14:09:23 +0000

    Improved text query

--- service/src/main/java/com/wmsi/sgx/service/search/elasticsearch/query/TextSearchQueryBuilder.java
+++ service/src/main/java/com/wmsi/sgx/service/search/elasticsearch/query/TextSearchQueryBuilder.java
@@ -1,6 +1,8 @@
 package com.wmsi.sgx.service.search.elasticsearch.query;
 
+import org.elasticsearch.index.query.MatchQueryBuilder.Type;
 import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.index.query.MatchQueryBuilder.Operator;
 import org.elasticsearch.search.builder.SearchSourceBuilder;
 
 public class TextSearchQueryBuilder extends AbstractQueryBuilder<String>{
@@ -9,11 +11,26 @@ public class TextSearchQueryBuilder extends AbstractQueryBuilder<String>{
 	
 	@Override
 	public SearchSourceBuilder getBuilder(String text) {
+		
 		return new SearchSourceBuilder()
-			.query(QueryBuilders.queryString(text)
-			.analyzer("sgx_text_ngram_analyzer")
-			.field("companyName")
-			.field("tickerSearch"))
-			.size(MAX_RESULTS);
+			.query(QueryBuilders.boolQuery()
+				// Text search on company Name - boosted for importance
+				.should(QueryBuilders.matchQuery("companyName", text)
+						.analyzer("sgx_text_ngram_analyzer")
+						.operator(Operator.AND)
+						.boost(3))
+				// Text search for tickers
+				.should(QueryBuilders.matchQuery("tickerSearch", text)
+						.analyzer("standard")
+						.operator(Operator.AND)
+						.boost(2))
+				
+				// Text search for prefix if only partial characters entered
+				.should(QueryBuilders.multiMatchQuery(text, "companyName", "tickerSearch")
+						.analyzer("standard")
+						.operator(Operator.AND)
+						.type(Type.PHRASE_PREFIX)))
+
+				.size(MAX_RESULTS);
 	}
 }
