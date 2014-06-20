e3c89626df3955f1347f34399222d3d4d4818535
michael_parcewski <michael_parcewski@c31ae540-8fef-4b06-900f-01f520cbaedc>
2014-06-20 02:43:29 +0000

    mergin with what's in production

--- trunk/phantom/server.js
+++ trunk/phantom/server.js
@@ -1,23 +1,18 @@
 var express = require('express');
 var nodePhantom = require('node-phantom');
 var app = express();
-var phantom = null, attempts = 0;
+var phantoms = [], running = [], maxProcs = 10;
 
 /**
  * start getting requests
  */
 app.get("/", function(req, res) {
-	
-	console.log(req.query.site);
-	
-	var retFN = function(err, page) { drawPage(req.query.site, res, err, page); };
-	
-	// initialize (need to pass render first time)
-	var ph = getPhantom(retFN);
-	
-	// if it's already set, run it
-	if (ph != null) ph.createPage(retFN);
-	    
+
+        console.log("URL: " + req.query.site);
+
+        // initialize (need to pass render first time)
+        getPhantom(req.query.site, res);
+
 });
 
 //start listening
@@ -25,102 +20,87 @@ app.listen(3000);
 
 /**
  * initialize phantom
+ * SGX servers are having issues pooling phantom (and even reusing instances)
+ * need to return a new one each time and restart regularly
  * @returns
  */
-function getPhantom(fn) {
-	if (phantom == null || attempts >= 50) {
-		attempts = 0;
-		if (phantom != null) phantom.close();
-		nodePhantom.create(function(err,ph) { 
-			phantom = ph; 
-			phantom.createPage(fn);
-		});	
-	}
-	return phantom;
+function getPhantom(site, res) {
+    nodePhantom.create(function(err,ph) {
+    	ph.createPage(function(err, page) { drawPage(site, res, err, page, ph); });
+    });
 }
 
-
 /**
- * handle rendering
- * @param url
- * @param response
- * @param err
- * @param page
- */
-function drawPage(url, response, err, page) {
-	
-    page.open(url, function(pErr,status) {
-    	
-    	attempts++;
-    	
-    	var myInterval = setInterval(
-    			function() {
-    				page.evaluate(
-    						function() {
-    							
-    							if (typeof document["pdf-name"] === "undefined") return false;
-    							
-    		    				var pageProps = new Object();
-    		    				pageProps.outputName = $("body").attr("pdf-name");
-    		    				pageProps.headerHTML = $(".header-content").html();
-    		    				pageProps.headerHeight = $(".header-content").outerHeight() + "px";
-    		    				pageProps.footerHTML = $(".footer-content").html();
-    		    				pageProps.footerHeight = $(".footer-content").outerHeight() + "px";
-    		    				pageProps.paperHeight = $("body").attr("paper-height");
-    		    				pageProps.paperWidth = $("body").attr("paper-width");
-    		    				pageProps.paperMargin = $.parseJSON($("body").attr("paper-margin"));
-    		    				$(".footer-content").hide();
-    		    				return pageProps;
-    		    				
-    						},
-    						function(err, pageProps) {
-    							
-    							if (!pageProps) return;
+* handle rendering
+* @param url
+* @param response
+* @param err
+* @param page
+*/
+function drawPage(site, response, err, page, ph) {
 
-    							clearInterval(myInterval);
-    							
-    							var paperSize = {
-    			    					height: pageProps.paperHeight,
-    			    					width: pageProps.paperWidth,
-    			    					margin: {
-    			    						top: pageProps.paperMargin[0],
-    			    						bottom: pageProps.paperMargin[1],
-    			    						left: pageProps.paperMargin[2],
-    			    						right: pageProps.paperMargin[3]
-    			    					}
-    							};
+	page.open(site, function(pErr,status) {
+		var myInterval = setInterval(
+			function() {
+				page.evaluate(
+						function() {
+							if (typeof document["pdf-name"] === "undefined") return false;
+                            var pageProps = new Object();
+                            pageProps.outputName = $("body").attr("pdf-name");
+                            pageProps.headerHTML = $(".header-content").html();
+                            pageProps.headerHeight = $(".header-content").outerHeight() + "px";
+                            pageProps.footerHTML = $(".footer-content").html();
+                            pageProps.footerHeight = $(".footer-content").outerHeight() + "px";
+                            pageProps.paperHeight = $("body").attr("paper-height");
+                            pageProps.paperWidth = $("body").attr("paper-width");
+                            pageProps.paperMargin = $.parseJSON($("body").attr("paper-margin"));
+                            $(".footer-content").hide();
+                            return pageProps;
+                        },
+                        function(err, pageProps) {
+                        	 if (!pageProps) return;
+                             clearInterval(myInterval);
+                             var paperSize = {
+                                 height: pageProps.paperHeight,
+                                     width: pageProps.paperWidth,
+                                     margin: {
+                                             top: pageProps.paperMargin[0],
+                                             bottom: pageProps.paperMargin[1],
+                                             left: pageProps.paperMargin[2],
+                                             right: pageProps.paperMargin[3]
+                                     }
+                             };
 
-        		    			if (typeof pageProps.headerHTML !== 'undefined') {
-        		    				paperSize.header = {
-    		    						height: pageProps.headerHeight,
-    		    						contents: "function(pageNum, numPages) { return decodeURI(\"" + encodeURI(pageProps.headerHTML) + "\"); }"
-        		    				};
-        		    			}
-        		    			
-        		    			if (typeof pageProps.footerHTML !== 'undefined') {
-        		    				paperSize.footer = {
-    		    						height: pageProps.footerHeight,
-    		    						contents: "function(pageNum, numPages) { return decodeURI(\"" + encodeURI(pageProps.footerHTML) + "\"); }"
-        		    				};
-        		    			}
-    							
-    							page.set('paperSize', paperSize, function(result) {
-            		    			page.render("pdfcache/" + pageProps.outputName, function(err, data) {
-        								response.download("pdfcache/" + pageProps.outputName);
-                		    			page.close();
-        							});
-    							});
-    							
-    						}
-    					);
-    			},
-    			500
-    			);
-    });
-   
-    /** handle logging */
-    page.onConsoleMessage = function(msg) {
-        console.log(msg);
-    };    
-    
+                             if (typeof pageProps.headerHTML !== 'undefined') {
+                                     paperSize.header = {
+                                             height: pageProps.headerHeight,
+                                             contents: "function(pageNum, numPages) { return decodeURI(\"" + encodeURI(pageProps.headerHTML) + "\"); }"
+                                     };
+                             }
+
+                             if (typeof pageProps.footerHTML !== 'undefined') {
+                                     paperSize.footer = {
+                                             height: pageProps.footerHeight,
+                                             contents: "function(pageNum, numPages) { return decodeURI(\"" + encodeURI(pageProps.footerHTML) + "\"); }"
+                                     };
+                             }
+
+                             page.set('paperSize', paperSize, function(result) {
+                        	 page.render("pdfcache/" + pageProps.outputName, function(err, data) {
+                             response.download("pdfcache/" + pageProps.outputName);
+                             page.close();
+                             ph.exit();
+                         });
+                     });
+                 });
+             },
+             500
+         );
+	});
+
+	/** handle logging */
+	page.onConsoleMessage = function(msg) {
+	 console.log(msg);
+	};
+
 }
