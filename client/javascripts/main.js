5e3172f4ecbb53ce8465a1b65b351b5c58bca0b0
michael_parcewski <michael_parcewski@c31ae540-8fef-4b06-900f-01f520cbaedc>
2014-08-19 13:31:38 +0000

    decache scripts

--- client/javascripts/main.js
+++ client/javascripts/main.js
@@ -1,7 +1,7 @@
 (function() {
 
     require({
-        urlArgs: "v=2.1.0",
+        urlArgs: "v=2.1.1",
         paths: {
             jquery: 'vendor/jquery-modern/jquery',
             jquicore: 'vendor/jquery.ui/ui/jquery.ui.core',
