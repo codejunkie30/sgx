6d897786e3f972e2d7b6ffa043d3e0325fa60677
michael_parcewski <michael_parcewski@c31ae540-8fef-4b06-900f-01f520cbaedc>
2014-08-25 13:35:06 +0000

    variety of fixes for phase 2

--- client/javascripts/main.js
+++ client/javascripts/main.js
@@ -1,7 +1,7 @@
 (function() {
 
     require({
-        urlArgs: "v=2.1.3",
+        urlArgs: "v=2.1.5",
         paths: {
             jquery: 'vendor/jquery-modern/jquery',
             jquicore: 'vendor/jquery.ui/ui/jquery.ui.core',
