31633c624431254d6995ab54ed7fe5947e722ebf
michael_parcewski <michael_parcewski@c31ae540-8fef-4b06-900f-01f520cbaedc>
2014-09-04 19:08:56 +0000

    lots of small changes

--- client/javascripts/main.js
+++ client/javascripts/main.js
@@ -1,7 +1,7 @@
 (function() {
 
     require({
-        urlArgs: "v=2.1.13",
+        urlArgs: "v=2.1.14",
         paths: {
             jquery: 'vendor/jquery-modern/jquery',
             jquicore: 'vendor/jquery.ui/ui/jquery.ui.core',
