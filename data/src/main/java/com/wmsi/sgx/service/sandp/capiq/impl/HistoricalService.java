6f5044c582aef70007a42345901837d45d496619
dwfwu <dwfwu@c31ae540-8fef-4b06-900f-01f520cbaedc>
2014-11-10 19:10:28 +0000

    3 queries for price History instead of 1

--- data/src/main/java/com/wmsi/sgx/service/sandp/capiq/impl/HistoricalService.java
+++ data/src/main/java/com/wmsi/sgx/service/sandp/capiq/impl/HistoricalService.java
@@ -22,7 +22,9 @@ import com.wmsi.sgx.util.DateUtil;
 @SuppressWarnings("unchecked")
 public class HistoricalService extends AbstractDataService {
 	
-	private ClassPathResource template = new ClassPathResource("META-INF/query/capiq/priceHistory.json");
+	private ClassPathResource template1 = new ClassPathResource("META-INF/query/capiq/priceHistory.json");
+	private ClassPathResource template2 = new ClassPathResource("META-INF/query/capiq/priceHistory2.json");
+	private ClassPathResource template3 = new ClassPathResource("META-INF/query/capiq/priceHistory3.json");
 	
 	@Override
 	public PriceHistory load(String id, String... parms) throws CapIQRequestException, ResponseParserException {
@@ -34,13 +36,15 @@ public class HistoricalService extends AbstractDataService {
 		ctx.put("startDate", parms[0]);
 		ctx.put("endDate", parms[1]);
 
-		CapIQResponse response = requestExecutor.execute(new CapIQRequestImpl(template), ctx);
+		CapIQResponse response1 = requestExecutor.execute(new CapIQRequestImpl(template1), ctx);
+		CapIQResponse response2 = requestExecutor.execute(new CapIQRequestImpl(template2), ctx);
+		CapIQResponse response3 = requestExecutor.execute(new CapIQRequestImpl(template3), ctx);
 
-		CapIQResult prices = response.getResults().get(0);
-		CapIQResult highprices = response.getResults().get(1);
-		CapIQResult lowprices = response.getResults().get(2);
-		CapIQResult openprices = response.getResults().get(3);
-		CapIQResult volumes = response.getResults().get(4);
+		CapIQResult prices = response1.getResults().get(0);
+		CapIQResult highprices = response1.getResults().get(1);
+		CapIQResult lowprices = response2.getResults().get(0);
+		CapIQResult openprices = response2.getResults().get(1);
+		CapIQResult volumes = response3.getResults().get(0);
 		
 		// Strip exchange extension from ticker if present
 		int exIndex = id.indexOf(':');
