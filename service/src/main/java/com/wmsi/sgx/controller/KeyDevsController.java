3316c54faef85906389766296970fbc99d5acecb
dwfwu <dwfwu@c31ae540-8fef-4b06-900f-01f520cbaedc>
2014-11-12 19:43:35 +0000

    Key devs query builder/controller/cache

---
+++ service/src/main/java/com/wmsi/sgx/controller/KeyDevsController.java
@@ -0,0 +1,29 @@
+package com.wmsi.sgx.controller;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.wmsi.sgx.model.KeyDevs;
+import com.wmsi.sgx.model.keydevs.KeyDevsRequest;
+import com.wmsi.sgx.service.KeyDevsService;
+import com.wmsi.sgx.service.ServiceException;
+import com.wmsi.sgx.service.search.SearchResult;
+
+@RestController
+@RequestMapping(method=RequestMethod.POST, produces="application/json")
+public class KeyDevsController{
+
+	@Autowired
+	private KeyDevsService keyDevsService;
+	
+	@RequestMapping("search/keydevs")
+	public SearchResult<KeyDevs> searchKeyDevs(@RequestBody KeyDevsRequest search) throws ServiceException{
+
+		return keyDevsService.search(search);
+	}
+	
+}
--- service/src/main/java/com/wmsi/sgx/service/search/elasticsearch/query/KeyDevsQueryBuilder.java
+++ service/src/main/java/com/wmsi/sgx/service/search/elasticsearch/query/KeyDevsQueryBuilder.java
@@ -1,5 +1,8 @@
 package com.wmsi.sgx.service.search.elasticsearch.query;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
 import org.elasticsearch.index.query.FilterBuilders;
 import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.search.builder.SearchSourceBuilder;
@@ -15,20 +18,37 @@ public class KeyDevsQueryBuilder extends AbstractQueryBuilder{
 	private KeyDevsRequest req; 
 	public KeyDevsQueryBuilder (KeyDevsRequest req) {
 		this.req = req;
+		
 	}
 	                
 	@Override
 	public String build(){
 		
-		return new SearchSourceBuilder()		
+		SearchSourceBuilder builder = new SearchSourceBuilder()		
 		    .query(QueryBuilders
 		    	.termQuery("_id", req.getTickerCode()))
-		    .postFilter(
-		    	FilterBuilders.rangeFilter("")
-		    	.to(req.getTo())
-		    	.from(req.getFrom()))
 		    	.size(MAX_RESULTS)
-		    	.sort("date", SortOrder.DESC)
-		    	.toString();
+		    	.sort("date", SortOrder.DESC);
+			
+		
+
+		if(req.getFrom() != null && req.getTo() != null)
+			builder.postFilter(
+			    	FilterBuilders.rangeFilter("keyDevs.date")
+			    	.from(getTime(req.getFrom()))
+			    	.to(getTime(req.getTo())));
+		
+		return builder.toString();
+	}
+	
+	private long getTime(Object object){
+		try{
+			SimpleDateFormat fmt = new SimpleDateFormat("yyyy-MM-dd");
+			return fmt.parse(object.toString()).getTime();
+		}
+		catch(ParseException e){
+			
+			return 0;
+		}
 	}
 }
