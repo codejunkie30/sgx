58cc8a314b7891827147c55d50b4d35d05fdf932
justin_lee <justin_lee@c31ae540-8fef-4b06-900f-01f520cbaedc>
2014-11-13 17:23:50 +0000

    Adding date filter for key devs.

--- service/src/main/java/com/wmsi/sgx/service/impl/KeyDevsServiceImpl.java
+++ service/src/main/java/com/wmsi/sgx/service/impl/KeyDevsServiceImpl.java
@@ -1,17 +1,19 @@
 package com.wmsi.sgx.service.impl;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
+import com.wmsi.sgx.model.KeyDev;
 import com.wmsi.sgx.model.KeyDevs;
 import com.wmsi.sgx.model.keydevs.KeyDevsRequest;
 import com.wmsi.sgx.service.KeyDevsService;
 import com.wmsi.sgx.service.ServiceException;
-import com.wmsi.sgx.service.search.SearchResult;
 import com.wmsi.sgx.service.search.SearchService;
 import com.wmsi.sgx.service.search.SearchServiceException;
-import com.wmsi.sgx.service.search.elasticsearch.query.KeyDevsQueryBuilder;
 
 @Service
 public class KeyDevsServiceImpl implements KeyDevsService{
@@ -21,10 +23,26 @@ public class KeyDevsServiceImpl implements KeyDevsService{
 
 	@Override
 	@Cacheable("keyDevsSearch")
-	public SearchResult<KeyDevs> search(KeyDevsRequest req) throws ServiceException {
+	public KeyDevs search(KeyDevsRequest req) throws ServiceException {
 
 		try{
-			return keyDevsSearch.search(new KeyDevsQueryBuilder(req), KeyDevs.class);
+			KeyDevs keyDevs = keyDevsSearch.getById(req.getTickerCode(), KeyDevs.class);
+			
+			List<KeyDev> filtered = new ArrayList<KeyDev>();
+			
+			for(KeyDev dev : keyDevs.getKeyDevs()){
+				Long devDate = dev.getDate().getTime();
+				
+				if(devDate.compareTo(Long.valueOf(req.getFrom().toString())) > 0 
+					&& devDate.compareTo(Long.valueOf(req.getTo().toString())) < 0){
+					
+					filtered.add(dev);
+				}		
+			}
+			
+			keyDevs.setKeyDevs(filtered);
+			
+			return keyDevs;
 		}
 		catch(SearchServiceException e){
 			throw new ServiceException("Query execution failed", e);
