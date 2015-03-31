1e86114090bb7501d63c8f5c70b3e3538475832b
michael_parcewski <michael_parcewski@c31ae540-8fef-4b06-900f-01f520cbaedc>
2015-03-31 12:35:28 +0000

    final production

--- trunk/client/javascripts/main.js
+++ trunk/client/javascripts/main.js
@@ -1,7 +1,7 @@
 (function() {
 
     require({
-        urlArgs: "v=2.1.19",
+        urlArgs: "v=2.2.1",
         paths: {
             jquery: 'vendor/jquery-modern/jquery',
             jquicore: 'vendor/jquery.ui/ui/jquery.ui.core',
