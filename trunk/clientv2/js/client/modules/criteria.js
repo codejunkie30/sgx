500bf5d50fec6cecb61241e297ef2b36701dbbac
michael_parcewski <michael_parcewski@c31ae540-8fef-4b06-900f-01f520cbaedc>
2015-06-24 16:16:12 +0000

    another round of updates

--- trunk/clientv2/js/client/modules/criteria.js
+++ trunk/clientv2/js/client/modules/criteria.js
@@ -41,6 +41,99 @@ define([ "wmsi/utils", "knockout", "text!client/data/fields.json" ], function(UT
     		*/
     	},
     	
+    	getDistributions: function(data, finished) {
+
+    		var tmpF = finished;
+    		var endpoint = this.screener.fqdn + "/sgx/search/distributions";
+    		var hasPercentChange = $.inArray("percentChange", data.fields);
+    		if (hasPercentChange != -1) data.fields.splice(hasPercentChange, 1);
+    		
+    		// change the function
+    		if (hasPercentChange != -1) {
+    			tmpF = function(data) { 
+    				data.distributions.push({ "field": "percentChange", buckets: [], template: "change" }); 
+    				finished(data);
+    			};
+    		}
+    		
+    		// do something else
+    		if (data.fields.length == 0) { 
+    			data = { distributions: [] }; 
+    			tmpF(data);
+    			return;
+    		}
+    		
+        	UTIL.handleAjaxRequest(endpoint, data, tmpF, undefined);
+
+    	},
+    	
+        handleDistributions: function(data) {
+        	
+        	if (typeof data.fieldValues === "undefined") return;
+        	
+        	$.each(data.fieldValues, function(idx, field) {
+
+        		var fieldData = CRITERIA.getFieldById(field.field);
+
+        		// force sort asc
+        		field.values.sort(function(a, b) { return a - b; });
+
+        		// get the type and count
+        		var type = fieldData.hasOwnProperty("distribution-type") ? fieldData["distribution-type"] : "normal";
+        		var bCount = fieldData.hasOwnProperty("distribution-buckets") ? fieldData["distribution-buckets"] : 75;
+
+        		// get the random distributions
+        		var buckets = {};
+        		$.each(field.values, function(vIdx, val) { CRITERIA.randomizeBucket(buckets, val, type, bCount); });
+        		
+        		// build the collection
+        		var arr = [], start = 0;
+            	for (var prop in buckets) {
+            		if (!buckets.hasOwnProperty(prop)) continue;
+            		var bucket = {};
+            		bucket.count = buckets[prop].length;
+            		bucket.from = field.values[start];
+            		bucket.to = field.values[(buckets[prop].length-1) + start];
+            		arr.push(bucket);
+            		start += buckets[prop].length;
+            	}
+            	
+            	fieldData.buckets = arr;
+        		
+        	});
+        	
+        	delete data.fieldValues;
+        	
+        },
+        
+        randomizeBucket: function(bucket, val, type, cnt) {
+        	var idx = (Math.round(val * 100000)%cnt);
+        	if (type == "log") idx = (Math.round(Math.log(val * 100000))%cnt);
+    		if (!bucket.hasOwnProperty(idx)) bucket[idx] = [];
+    		bucket[idx].push(val);
+        },
+        
+    	getSelectedFields: function() {
+    		var data = { fields: [] };
+    		$(".panel .checkbox.checked").each(function(idx, field) {
+    			data.fields.push($(field).attr("data-id"));
+    		});
+    		return data;
+    	},
+    	
+    	getFieldById: function(id) {
+    		var ret = null;
+    		var order = 0;
+    		$(this.fieldGroups).each(function(idx, grp) {
+    			$(grp.fields).each(function(f, field) {
+					field.order = order;
+    				if (field.id == id) ret = field;
+    				order++;
+    			});
+    		});
+    		return ret;
+    	},
+    	
     	maxCriteriaMsg: function() {
     		
             this.screener.modal.open({
@@ -49,6 +142,62 @@ define([ "wmsi/utils", "knockout", "text!client/data/fields.json" ], function(UT
             });
     		
     	},
+    	
+        renderInputs: function(data) {
+
+        	this.handleDistributions(data);
+        	
+        	var runsearch = true;
+        	
+        	if (data.distributions.length > 1) {
+            	data.distributions.sort(function(a, b) {
+            		return CRITERIA.getFieldById(a.field).order - CRITERIA.getFieldById(b.field).order;
+            	});
+        	}
+        	
+        	$.each(data.distributions, function(idx, distribution) {
+
+        		var field = CRITERIA.getFieldById(distribution.field);
+        		
+        		if (field == null) return;
+        		
+        		var el = $("<tr >").attr("data-bind", "template: 'criteria-" + field.template + "'").addClass("criteria");
+        		$(el).appendTo(".search-criteria tbody");
+        		ko.applyBindings(field, $(el)[0]);
+
+        		/**
+        		// draw the appropriate input
+        		if (distribution.template == "select") template = SGX.screener.drawCriteriaSelect(distribution);
+        		else if (distribution.template == "change") {
+        			template = SGX.screener.drawCriteriaChange(distribution);
+        			runsearch = false;
+        		}
+        		else template = SGX.screener.drawCriteriaSlider(distribution);
+        		
+            	$(".label .glossary-item", template).attr("glossary-key", distribution.id);
+            	$(template).attr("data-name", distribution.id);
+
+        		$("td.remove", template).click(function(e) {
+                    e.stopPropagation();
+                    e.preventDefault();
+                    SGX.tooltip.close();
+                    SGX.screener.criteriaChange.removeCriteria($(this).closest(".criteria"), function() {});
+        		});
+        		
+        		SGX.tooltip.init(template);
+        		*/
+        		
+        		
+        	});
+        	
+        	/**
+        	$(".search-criteria .criteria").removeClass("even");
+        	$(".search-criteria .criteria:even").addClass("even");
+        	SGX.formatter.formatElements(".search-criteria");
+        	*/
+        	
+        	return runsearch;                	
+        },
     	
     	clickEvents: {
     		
--- trunk/clientv2/js/wmsi/utils.js
+++ trunk/clientv2/js/wmsi/utils.js
@@ -125,29 +125,6 @@ define(["jquery"], function($) {
     	},
     	
     	/**
-    	 * don't allow inputs with selector to input anything except numbers
-    	 */
-    	numericOnly: function(selector) {
-    		
-		    $(selector).keydown(function (e) {
-		        // Allow: backspace, delete, tab, escape, enter and .
-		        if ($.inArray(e.keyCode, [46, 8, 9, 27, 13, 110, 190]) !== -1 ||
-		             // Allow: Ctrl+A
-		            (e.keyCode == 65 && e.ctrlKey === true) || 
-		             // Allow: home, end, left, right
-		            (e.keyCode >= 35 && e.keyCode <= 39)) {
-		                 // let it happen, don't do anything
-		                 return;
-		        }
-		        // Ensure that it is a number and stop the keypress
-		        if ((e.shiftKey || (e.keyCode < 48 || e.keyCode > 57)) && (e.keyCode < 96 || e.keyCode > 105)) {
-		            e.preventDefault();
-		        }
-		    });
-    		
-    	},
-    	
-    	/**
     	 * convert to SGT
     	 * @param dt current date
     	 * @returns {Date} in proper TZ
