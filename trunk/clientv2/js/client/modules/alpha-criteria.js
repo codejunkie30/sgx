ac9b09e4e0de1489310627a0de2e07e1cb463884
michael_parcewski <michael_parcewski@c31ae540-8fef-4b06-900f-01f520cbaedc>
2015-06-29 17:26:56 +0000

    finishing up different screener options

--- trunk/clientv2/js/client/modules/alpha-criteria.js
+++ trunk/clientv2/js/client/modules/alpha-criteria.js
@@ -1,24 +1,87 @@
-define([ "wmsi/utils", "knockout" ], function(UTIL, ko) {
+define([ "wmsi/utils", "knockout", "text!client/data/factors.json" ], function(UTIL, ko, FACTORS) {
 
+	/**
+	 * allows us to customize particular criteria
+	 * assumes working with criteria object for model
+	 */
+	ko.bindingHandlers.createFactor = {
+	    init: function(element, valueAccessor, allBindings, viewModel, bindingContext) {
+	    	
+	    	$.each(new Array(5), function(quint) {
+	    		
+	    		var span = $("<span />").attr("data-quintile", quint + 1).appendTo($(".quintiles", element));
+	    		
+				$(span).hover(
+						function(e) {
+							var mine = $(this).attr("data-quintile");
+							$(this).parent().removeClass(function(pos, css) { return (css.match(/(^|\s)per-\S+/g) || []).join(' '); });
+							$(this).parent().addClass("per-" + (mine*20));
+						},
+						function(e) {
+							var mine = $(this).attr("data-quintile");
+							$(this).parent().removeClass(function(pos, css) { return (css.match(/(^|\s)per-\S+/g) || []).join(' '); });
+							if (typeof $(this).parent().attr("data-class") !== "undefined") $(this).parent().addClass($(this).parent().attr("data-class"));
+				});
+				
+				$(span).click(function(e) {
+					var mine = $(this).attr("data-quintile");
+					$(this).closest(".quintiles").addClass("per-" + (mine*20)).attr("data-class", "per-" + (mine*20));
+					$(this).closest(".factor").attr("data-value", mine);
+					$(".right-label", $(this).closest(".factor")).addClass("selected");
+				});
+				
+	    	});
+	    	
+			$(".right-label", element).click(function() {
+				if (!$(this).hasClass("selected")) return;
+				$(this).removeClass("selected");
+				var prg = $(".quintiles", element);
+				$(prg).removeClass($(prg).attr("data-class")).removeAttr("data-class")
+				$(element).removeAttr("data-value");
+			});
+	    	
+	    }
+	};
+	
 	var CRITERIA = {
 			
 		screener: null,
+		
+		factorDefaults: { "minLabel": "High", "maxLabel": "Low"  },
 			
 		init: function(screener, finalize) {
+			
+			var tmp = JSON.parse(FACTORS);
+			$.each(tmp.factors, function(idx, field) {
+				var item = { "minLabel": "High", "maxLabel": "Low"  };
+				$.extend(item, field);
+				tmp.factors[idx] = item;
+			});
+			
+			$.extend(true, this, tmp);
 			
 			this.screener = screener;
 			screener.criteria = this;
 			
-			finalize(undefined);
+			if ($(".search-options[data-section='alpha-factors'] .alpha-factors[data-init='true']").length == 0) {
+				ko.applyBindings(this, $(".search-options[data-section='alpha-factors']")[0]);
+				$(".search-options[data-section='alpha-factors'] .alpha-factors").attr("data-init", "true");
+				finalize(undefined);
+				return;
+			}
+			
+			this.reset(finalize);
 			
 		},
 		
 		renderInputs: function(data) {
-			// TODO
+			// NOTHING NEEDS TO HAPPEN
 		},
 
     	reset: function(finished) {
-        	// TODO
+
+    		$(".alpha-factors .right-label").click();
+    		
     	}
 		
 	};
