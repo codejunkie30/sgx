fe023591a894856046abfd6ef2cab312df46389b
michael_parcewski <michael_parcewski@c31ae540-8fef-4b06-900f-01f520cbaedc>
2015-07-01 22:04:15 +0000

    customize results and more results working

--- trunk/clientv2/js/client/modules/results.js
+++ trunk/clientv2/js/client/modules/results.js
@@ -1,7 +1,12 @@
-define([ "wmsi/utils", "knockout", "text!client/data/fields.json", "knockout-repeat" ], function(UTIL, ko, fieldData) {
+define([ "wmsi/utils", "knockout", "text!client/data/fields.json", "text!client/templates/customize-display.html", "knockout-repeat" ], function(UTIL, ko, fieldData, customizeHTML) {
 	
-	
-	
+	ko.bindingHandlers.formatResultColumn = {
+	    init: function(element, valueAccessor, allBindings, viewModel, bindingContext) {
+	    	var models = ko.utils.unwrapObservable(valueAccessor());
+	    	var val = RESULTS.formatField(viewModel, models.row, models.prop);
+	    	$(element).text(val);
+	    }
+	};
 	
 	var RESULTS = {
 			
@@ -60,15 +65,90 @@ define([ "wmsi/utils", "knockout", "text!client/data/fields.json", "knockout-rep
 			
 		},
 		
+		formatField: function(field, row, id) {
+			
+			if (!row.hasOwnProperty(id) || row[id] == null) return;
+			
+	    	var val = row[id];
+	    	
+    		if (field.hasOwnProperty("formatter")) {
+    			
+    			// handle list lookup
+    			if (field.formatter.type == "round-list") {
+    				val = val | 0;
+    				val = field.formatter.values[val];
+    			}
+
+    			// needs a minimum value
+    			if (field.formatter.hasOwnProperty("minField") && row.hasOwnProperty(field.formatter.minField)) {
+    				if (row[field.formatter.minField] < field.formatter.minValue) val = null;
+    			}
+    			
+    		}
+    		
+			var fmt = field.format == "millions" ? "number1" : field.format;
+			val = this.screener.getFormatted(fmt, val);
+    		
+    		return val;
+			
+		},
+		
+		customizeDisplay: function() {
+			
+			var settings = {
+	    			
+	                maxWidth: 700,
+	                
+                    content: customizeHTML,
+                    
+                    viewModel: this.viewModel,
+                    
+                    type: 'prompt',
+                    
+                    postLoad: function(settings) {
+                    	
+                    	ko.applyBindings(settings.viewModel, $(".modal-container")[0]);
+                    	
+                    	$(".modal-container .checkbox").click(function(e) {
+                    		
+                    		// already checked
+                    		if ($(this).is(".checked")) {
+                    			$(this).removeClass("checked");
+                    		}
+                    		else {
+
+                        		// can't check anymore
+                        		if ($(".modal-container .checked").length >= 4) { alert("Please remove a column before adidng a new one."); return; }
+                        		
+                        		// check it
+                        		$(this).addClass("checked");	
+
+                    		}
+                    		
+                    		var mdl = ko.dataFor($(this)[0]);
+                    		mdl.dataDisplay = $(this).is(".checked");
+                    		settings.viewModel.headersChanged(true);                    		
+                    		
+                    	});
+                    	
+                    }
+			};
+			
+			this.screener.modal.open(settings);
+			
+		},
+		
 		initModel: function(groups) {
 			
 			var mdl = {
+				screener: this.screener,
 				companies: ko.observable(null),
 				fieldGroups: groups,
 				staticFields: [ { "id":"companyName", "name":"Company Name", "format":"string" }, { "id":"tickerCode", "name":"Code", "format":"string" } ],
 				resultSize: ko.observable(30),
 				search: false,
-				page: ko.observable(1).extend({ withPrevious: 1 })
+				page: ko.observable(1).extend({ withPrevious: 1 }),
+				headersChanged: ko.observable(false)
 			};
 			
 			/**
@@ -77,9 +157,14 @@ define([ "wmsi/utils", "knockout", "text!client/data/fields.json", "knockout-rep
 			mdl.staticFields.push.apply(mdl.staticFields, groups[groups.length-1].fields.slice());
 			mdl.fieldGroups.remove(groups[groups.length-1]);
 			mdl.headers = ko.computed(function() {
+				this.headersChanged();
+				this.headersChanged(false);
 				var arr = this.staticFields.slice();
 				$.each(this.fieldGroups, function(idx, group) {
-					var defs = $.grep(group.fields, function(obj, idx) { return obj.hasOwnProperty("isDefault") && obj.isDefault; });
+					var defs = $.grep(group.fields, function(obj, idx) {
+						if (!obj.hasOwnProperty("dataDisplay") && obj.hasOwnProperty("isDefault") && obj.isDefault) obj.dataDisplay = true;
+						return obj.hasOwnProperty("dataDisplay") && obj.dataDisplay; 
+					});
 					if (defs.length > 0) arr.push.apply(arr, defs);
 				});
 				return arr;				
@@ -100,8 +185,10 @@ define([ "wmsi/utils", "knockout", "text!client/data/fields.json", "knockout-rep
 			 * paging
 			 * 
 			 */
-			mdl.page.subscribe(function(change) { 
-				console.log(">>" + change); 
+			mdl.changePage = function(i) { mdl.page(mdl.page() + i); }
+			
+			mdl.page.subscribe(function(change) {
+				console.log(change);
 				if (isNaN(change) || change <= 0 || change > mdl.pages()) { 
 					this.page(this.page.previous()); 
 					$(".pager input").focus();
