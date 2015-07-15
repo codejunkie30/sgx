547ca913d5f490ffd882a6aafec1b556afa8c667
michael_parcewski <michael_parcewski@c31ae540-8fef-4b06-900f-01f520cbaedc>
2015-07-15 17:34:50 +0000

    added delayed pricing, financials button and gti

--- clientv2/js/client/tearsheet.js
+++ clientv2/js/client/tearsheet.js
@@ -28,6 +28,7 @@ define([ "wmsi/utils", "knockout", "client/modules/price-chart" ], function(UTIL
 		ticker: UTIL.getParameterByName("code"),
 		
 		profileTab: null,
+		priceData: ko.observable({}),
 		
 		initPage: function() {
 
@@ -37,6 +38,14 @@ define([ "wmsi/utils", "knockout", "client/modules/price-chart" ], function(UTIL
 			var endpoint = this.fqdn + "/sgx/company";
 			var params = { id: this.ticker };
 			UTIL.handleAjaxRequest(endpoint, params, function(data) { var parent = self; parent.initCompanyData(data); }, undefined);
+			
+			http://sgx-api-lb-195267723.ap-southeast-1.elb.amazonaws.com
+			
+    		// init real-time/delayed pricing data
+			endpoint = "http://sgx-api-lb-195267723.ap-southeast-1.elb.amazonaws.com/sgx/price"; 
+    		//endpoint = this.fqdn + "/sgx/price";
+    		params = { id: this.ticker };
+    		UTIL.handleAjaxRequest(endpoint, params, function(data) { var parent = self; parent.initPriceData(parent, data); }, undefined);
 			
     		return this;
 		},
@@ -60,6 +69,26 @@ define([ "wmsi/utils", "knockout", "client/modules/price-chart" ], function(UTIL
 
     		// resize
     		this.resizeIframeSimple();
+			
+		},
+		
+		initPriceData: function(parent, data) {
+			
+    		var dateField = data.price.hasOwnProperty("lastTradeTimestamp") ? data.price.lastTradeTimestamp : data.price.previousDate;
+    		var date = Date.fromISO(dateField);
+    		var price = data.price.hasOwnProperty("lastPrice") ? data.price.lastPrice : data.price.closePrice;
+    		
+    		if (typeof price === "undefined" || price == null) return;
+    		
+    		var pdata = { 
+    			'price': price, 
+    			'currency': data.price.tradingCurrency,  
+    			'change': data.price.change,
+    			'day': $.datepicker.formatDate( "dd/M/yy", date),
+    			'time': date.getHours() + ":" + String("00" + date.getMinutes()).slice(-2) + ""
+    		};
+    		
+    		parent.priceData(pdata);
 			
 		},
 		
