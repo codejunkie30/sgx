d340ee93e6791c59f7fe38bfac00ecd92fec64f9
justin_lee <justin_lee@c31ae540-8fef-4b06-900f-01f520cbaedc>
2015-08-10 18:03:23 +0000

    Removing real time pricing code. Migrated to sgx-pricing.

--- service/src/main/java/com/wmsi/sgx/service/quanthouse/QuanthouseService.java
+++
@@ -1,8 +0,0 @@
-package com.wmsi.sgx.service.quanthouse;
-
-import com.wmsi.sgx.model.Price;
-
-public interface QuanthouseService {
-
-	Price getPrice(String market, String id)throws QuanthouseServiceException;	
-}
