2aa7679b84dcbbb4876437b87c3056f622477f6c
michael_parcewski <michael_parcewski@c31ae540-8fef-4b06-900f-01f520cbaedc>
2015-08-28 19:55:18 +0000

    last round, minus archiving

--- trunk/xf-export/XFDataDump/XFDataDump/Properties/AssemblyInfo.cs
+++ trunk/xf-export/XFDataDump/XFDataDump/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("a861f4e4-b7d1-4753-beb1-77e2ca97d690")]
+[assembly: Guid("79f99484-42ac-44f9-9cae-5fc1ca03c3a7")]
 
 // Version information for an assembly consists of the following four values:
 //
