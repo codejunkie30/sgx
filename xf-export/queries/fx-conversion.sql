d6ee3ceceba5aa4984554f2ffe001d3fdef8da49
michael_parcewski <michael_parcewski@c31ae540-8fef-4b06-900f-01f520cbaedc>
2015-08-31 18:42:30 +0000

    standardizing routines, fixing export functions and improving performance

--- xf-export/queries/fx-conversion.sql
+++ xf-export/queries/fx-conversion.sql
@@ -1,3 +1,5 @@
+select distinct fromCurr, toCurr, pricingDate, multiplicationFactor
+ from (
 select fromC.ISOCode as fromCurr, toC.ISOCode as toCurr, fromEx.pricingDate, toEx.currencyRateClose/fromEx.currencyRateClose as multiplicationFactor
 from ciqExchangeRate fromEx
 join ciqCurrency fromC on fromEx.currencyId=fromC.currencyId
@@ -6,10 +8,11 @@ join ciqCurrency toC on toEx.currencyId=toC.currencyId
 where year(fromEx.PricingDate) >= year(GetDate()) - 7 --Limiting History
   and toEx.currencyId IN (SELECT distinct pop.currencyId FROM ##sgxpop pop) 
 UNION
-select fromC.ISOCode as fromCurr, toC.ISOCode as toCurr, fromEx.pricingDate, toEx.currencyRateClose/fromEx.currencyRateClose as multiplicationFactor
+select toC.ISOCode as toCurr, fromC.ISOCode as fromCurr, toEx.pricingDate, fromEx.currencyRateClose/toEx.currencyRateClose as multiplicationFactor 
 from ciqExchangeRate fromEx
 join ciqCurrency fromC on fromEx.currencyId=fromC.currencyId
 join ciqExchangeRate toEx on fromEx.pricingDate=toEx.pricingDate
 join ciqCurrency toC on toEx.currencyId=toC.currencyId
 where year(fromEx.PricingDate) >= year(GetDate()) - 7 --Limiting History
   and fromEx.currencyId IN (SELECT distinct pop.currencyId FROM ##sgxpop pop)
+) as tmp
