b0b9c9142bc047e50b8455d81a9957187c24f26c
scott <scott@c31ae540-8fef-4b06-900f-01f520cbaedc>
2015-09-29 17:47:23 +0000

    added header to company-comparables.html

--- clientv2/js/client/company-comparables.js
+++ clientv2/js/client/company-comparables.js
@@ -1,8 +1,10 @@
 define([ "wmsi/utils", "knockout", "client/modules/results", "client/modules/tearsheet" ], function(UTIL, ko, RESULTS, TS) {
 
 	var CCOMP = {
-			
-		results: null,
+		results: null,		
+		premiumUser: ko.observable(),	
+		premiumUserEmail: ko.observable(),		
+		premiumUserAccntInfo: ko.observable(),
 		
 		initPage: function() {
 			
@@ -16,6 +18,7 @@ define([ "wmsi/utils", "knockout", "client/modules/results", "client/modules/tea
 			var self = this;
 			this.init(function() { self.finish(self); });
 			
+			this.checkStatus();
 		},
 		
 		finish: function(me) {
@@ -29,6 +32,37 @@ define([ "wmsi/utils", "knockout", "client/modules/results", "client/modules/tea
     		var params = { id: me.ticker };
     		me.results.retrieve(endpoint, params, undefined, function() { return 0 });
 
+		},
+		checkStatus: function(){
+			
+			var endpoint = PAGE.fqdn + "/sgx/account/info";
+			var postType = 'POST';
+			var params = {};
+			var jsonp = 'callback';
+			var jsonpCallback = 'jsonpCallback';
+			
+			UTIL.handleAjaxRequest(
+				endpoint,
+				postType,
+				params,
+				jsonp,
+				function(data, textStatus, jqXHR){
+					if (data.reason == 'Full authentication is required to access this resource'){
+						PAGE.premiumUser(false);
+					} else {
+						PAGE.premiumUser(true);
+						PAGE.premiumUserAccntInfo = data;
+						PAGE.premiumUserEmail(PAGE.premiumUserAccntInfo.email);
+						PAGE.timedLogout();
+					}
+					
+				}, 
+				function(jqXHR, textStatus, errorThrown){
+					console.log('fail');
+					console.log(textStatus);
+					console.log(errorThrown);
+					console.log(jqXHR);
+				},jsonpCallback);			
 		}
 
 	};
