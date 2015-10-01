c48f3d4188843caddb45b1939c43984b2ef11a34
ksethi <ksethi@c31ae540-8fef-4b06-900f-01f520cbaedc>
2015-10-01 20:45:44 +0000

    Turning off CustomLoginFilter for testing purpose, will revert in few hours

--- service/src/main/java/com/wmsi/sgx/web/filter/JsonpWrappingFilter.java
+++ service/src/main/java/com/wmsi/sgx/web/filter/JsonpWrappingFilter.java
@@ -34,16 +34,51 @@ public class JsonpWrappingFilter extends OncePerRequestFilter{
 		log.debug("Jsonp Wrapper Filter invoked");
 		
 		Map<String, String[]> parms = request.getParameterMap();
-		
+
 		if(request.getMethod() == "GET" && parms.containsKey(JSON_PARAM_NAME) && parms.containsKey(JSONP_CALLBACK_PARAM)){
 		
 			log.debug("Converting jsonp GET request to POST with body");
-			JsonpWrappingFilterHelper jsonpWrappingFilterHelper = new JsonpWrappingFilterHelper();
-			jsonpWrappingFilterHelper.convertGETtoPOST(request, response, filterChain, parms);
+
+			// Convert json query string value to request body
+			GetToPostRequestWrapper postRequestWrapper = 
+					new GetToPostRequestWrapper(request, JSON_PARAM_NAME, MediaType.APPLICATION_JSON_VALUE);
+			
+			// Execute request, writing response to wrapper so we can manipulate the results.
+			GenericResponseWrapper wrapper = new GenericResponseWrapper(response);
+			filterChain.doFilter(postRequestWrapper, wrapper);
+			
+			String callback = parms.get(JSONP_CALLBACK_PARAM)[0];
+			
+			// Make sure callback parameter hasn't been compromised.  
+			if(!validateJson(callback))
+				throw new ServletException("Invalid characters found in jsonP callback parmameter. Possible XSS attempt");
+			
+			// Wrap json with jsonp callback
+			OutputStream out = response.getOutputStream();
+			
+			out.write((callback + "(").getBytes());
+			out.write(wrapper.getData());
+			out.write(");".getBytes());
+
+			wrapper.setContentType("text/javascript;UTF-8");
+
+			out.close();
+			
+			log.debug("Jsonp request converted successfully");
 		}
 		else{			
 			// Normal request
 			filterChain.doFilter(request, response);
 		}
-	}	
+	}
+
+	// Regex to check json for any non word characters or javascript keywords
+	private Pattern validJsonPattern = Pattern.compile("[^\\w\\$]|(alert|abstract|boolean|break|byte|case|catch|char|class|const|continue|debugger|default|delete|do|double|else|enum|export|extends|false|final|finally|float|for|function|goto|if|implements|import|in|instanceof|int|interface|long|native|new|null|package|private|protected|public|return|short|static|super|switch|synchronized|this|throw|throws|transient|true|try|typeof|var|volatile|void|while|with|NaN|Infinity|undefined)");
+	
+	/**
+	 * Validate json against injection attacks
+	 */
+	private boolean validateJson(String json){
+		return !validJsonPattern.matcher(json).find();
+	}
 }
