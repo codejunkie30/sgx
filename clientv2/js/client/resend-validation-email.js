f8ac1785de4eb071bda82588fcafecc8641e77c4
scott <scott@c31ae540-8fef-4b06-900f-01f520cbaedc>
2015-10-07 13:52:45 +0000

    checking in resend

---
+++ clientv2/js/client/resend-validation-email.js
@@ -0,0 +1,78 @@
+define([ "wmsi/utils", "knockout", "knockout-validate", "text!client/data/messages.json", "jquery-placeholder" ], function(UTIL, ko, validation, MESSAGES) {
+	
+	var RESEND = {
+		email: ko.observable(),
+		messages: JSON.parse(MESSAGES),
+		initPage: function() {
+						
+			this.isFormValid = ko.computed(function() {
+			    return this.email();
+			}, this);
+			
+			var displayMessage = RESEND.messages.messages[0];
+			
+    		// finish other page loading
+    		ko.applyBindings(this, $("body")[0]);
+			
+			ko.validation = validation;
+    		validation.init({ insertMessages: false });
+			
+			ko.validation.registerExtenders();
+			
+			RESEND.email.extend({
+				required: { message: displayMessage.emailRequired },
+				email: { message: displayMessage.emailValid }
+			});
+
+			this.errors = validation.group(this);			
+			
+			this.errors.subscribe(function () {
+				PAGE.resizeIframeSimple();
+			});
+						
+			// resize
+    		this.resizeIframeSimple();
+			
+			// Placeholder
+			$('.form input').placeholder();
+    		return this;
+		},
+		resendEmail: function(me){
+			var endpoint = me.fqdn + "/sgx/user/resetToken";
+			var postType = 'POST';
+			var params = { username: RESEND.email() };
+			var jsonp = 'callback';
+			var jsonpCallback = 'jsonpCallback';
+			
+			var displayMessage = RESEND.messages.messages[0];
+			
+			if (this.errors().length > 0 || this.isFormValid() == undefined) {				
+	            return
+	        }
+			
+			UTIL.handleAjaxRequest(
+				endpoint,
+				postType,
+				params, 
+				jsonp,
+				function(data, textStatus, jqXHR){
+					console.log(data);
+					if (data == true){
+						$('.form').empty().addClass('rp-sent');
+						$('<p/>').html(displayMessage.resendValidation.emailResent).appendTo('.form.rp-sent');
+						PAGE.resizeIframeSimple();	
+					}
+				}, 
+				function(jqXHR, textStatus, errorThrown){
+					console.log('fail');
+					console.log(textStatus);
+					console.log(errorThrown);
+					console.log(jqXHR);
+				},jsonpCallback);
+			
+		}
+	};
+	
+	return RESEND;
+	
+});
