81dc1e41a480e7c88b39c7cce8e97eee41646295
michael_parcewski <michael_parcewski@c31ae540-8fef-4b06-900f-01f520cbaedc>
2015-10-13 13:19:35 +0000

    make the war names and current build unique names per project

--- cron/pricing-deploy.sh
+++ cron/pricing-deploy.sh
@@ -1,7 +1,7 @@
 ## dynamic (per server) variables
-REMOTE_PATH="/mnt/100g/sgx-builds/api-premium"
-ENVIRONMENT_NAME="SGX Premium API (QA)"
-WAR_NAME="sgx.war"
+REMOTE_PATH="/mnt/100g/sgx-builds/pricing-premium"
+ENVIRONMENT_NAME="SGX Premium Pricing (QA)"
+WAR_NAME="sgx-pricing.war"
 
 ## move to cron directory
 SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
