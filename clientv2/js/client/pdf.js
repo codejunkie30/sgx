7b26d2845628f70f89821c953b81f5f5741ef8a7
scott <scott@c31ae540-8fef-4b06-900f-01f520cbaedc>
2015-10-21 18:58:18 +0000

    Updates to PDF's. Added pages for account purchase and account confirmation. Update to terms & conditions

--- clientv2/js/client/pdf.js
+++ clientv2/js/client/pdf.js
@@ -59,11 +59,8 @@ define(["wmsi/utils", "knockout", "client/company-financials", "client/company-t
 			var finendpoint = me.fqdn + "/sgx/company/financials";
     		var params = { id: me.ticker };
     		UTIL.handleAjaxRequest(finendpoint, postType, params, undefined, function(data) { me.initFinancials(me, data);  }, undefined, undefined);
-this.resizeIframeSimple();
-			return this;
-			
-			
-			
+			this.resizeIframeSimple();
+			return this;			
 		},
 		initPriceChart: function(parent, data) {
 			var finished = function() {
