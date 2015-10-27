59da1726838bd1cd43d0874b8fb9af17706cf400
ksethi <ksethi@c31ae540-8fef-4b06-900f-01f520cbaedc>
2015-10-27 13:30:28 +0000

    Adding Cron job for marking account expired after expiration period

--- service/src/main/java/com/wmsi/sgx/service/account/impl/AccountExpirationServiceImpl.java
+++ service/src/main/java/com/wmsi/sgx/service/account/impl/AccountExpirationServiceImpl.java
@@ -1,19 +1,20 @@
  package com.wmsi.sgx.service.account.impl;
 
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.MessageSource;
-import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import com.wmsi.sgx.domain.Account;
+import com.wmsi.sgx.domain.User;
 import com.wmsi.sgx.repository.AccountRepository;
 import com.wmsi.sgx.service.account.AcccountExiprationService;
+import com.wmsi.sgx.service.account.UserService;
 
 @Service
 public class AccountExpirationServiceImpl implements AcccountExiprationService{
@@ -26,13 +27,21 @@ public class AccountExpirationServiceImpl implements AcccountExiprationService{
 	@Autowired
 	private AccountRepository accountRepository;
 	
-
+	@Autowired
+	private UserService userRepository;
+	
 	//need to switch it to pick value from the placeholder
 	//there is some issue its not picking up value form there 
-	//@Scheduled(cron="0/25 * * * * ?")
+	@Scheduled(cron="0 39 9 ? * *")
 	public void checkAccountExpiration() {
 		List<Account> accounts = accountRepository.findAll();
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
+		
+		//Username Password does not exist its only for faking user to system default
+		UsernamePasswordAuthenticationToken authRequest = new UsernamePasswordAuthenticationToken("example@gmail.com",
+				"Test1234@");
+		
+		SecurityContextHolder.getContext().setAuthentication(authRequest);
 		
 		for( Account acc: accounts)	{
 			if(acc.getAlwaysActive() == false){
