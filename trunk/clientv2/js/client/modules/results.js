cba42d36c660af18364da3efb99d618a0593d50f
prai <prai@c31ae540-8fef-4b06-900f-01f520cbaedc>
2015-10-29 18:23:33 +0000

    removed console.log

--- trunk/clientv2/js/client/modules/results.js
+++ trunk/clientv2/js/client/modules/results.js
@@ -92,7 +92,7 @@ define([ "wmsi/utils", "knockout", "text!client/data/fields.json", "text!client/
 			
 			// resize
 			//PAGE.resizeIframe(PAGE.getTrueContentHeight(), -1);
-      console.log(this);
+
       PAGE.resizeIframeSimple();
 			PAGE.hideLoading();
 		},
