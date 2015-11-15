bcf35051e4298f87e89d7e0460b6d076de1726db
michael_parcewski <michael_parcewski@c31ae540-8fef-4b06-900f-01f520cbaedc>
2015-11-15 01:57:06 +0000

    incrementing version

--- clientv2/js/config.js
+++ clientv2/js/config.js
@@ -1,7 +1,7 @@
 (function() {
 
     require({
-        urlArgs: "v=1.16",
+        urlArgs: "v=1.18",
         paths: {
             "jquery": 'vendor/jquery/jquery.1.11.3.min',
             "jquery-ui": 'vendor/jquery/jquery-ui-1.11.4/jquery-ui.min',
