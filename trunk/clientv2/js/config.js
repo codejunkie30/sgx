e695e03f08c60c68fb474d7a5ba32f37b39eb960
michael_parcewski <michael_parcewski@c31ae540-8fef-4b06-900f-01f520cbaedc>
2015-11-25 11:29:54 +0000

    increment

--- trunk/clientv2/js/config.js
+++ trunk/clientv2/js/config.js
@@ -1,7 +1,7 @@
 (function() {
 
     require({
-        urlArgs: "v=1.33",
+        urlArgs: "v=1.34",
         paths: {
             "jquery": 'vendor/jquery/jquery.1.11.3.min',
             "jquery-ui": 'vendor/jquery/jquery-ui-1.11.4/jquery-ui.min',
