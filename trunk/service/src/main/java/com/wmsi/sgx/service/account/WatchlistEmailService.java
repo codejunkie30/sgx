01997e2654ce16b533ec3d33b76b6e77c0621916
ksethi <ksethi@c31ae540-8fef-4b06-900f-01f520cbaedc>
2015-11-30 21:12:08 +0000

    Adding a new Quartz Trigger for WatchList

--- trunk/service/src/main/java/com/wmsi/sgx/service/account/WatchlistEmailService.java
+++ trunk/service/src/main/java/com/wmsi/sgx/service/account/WatchlistEmailService.java
@@ -8,16 +8,20 @@ import javax.mail.MessagingException;
 import com.wmsi.sgx.domain.Account;
 import com.wmsi.sgx.domain.User;
 import com.wmsi.sgx.model.AlertOption;
+import com.wmsi.sgx.model.WatchlistModel;
 import com.wmsi.sgx.service.CompanyServiceException;
 import com.wmsi.sgx.service.search.SearchServiceException;
 
 public interface WatchlistEmailService {
 
-	void getWatchlistEmails()
+	/*void getWatchlistEmails()
 			throws QuanthouseServiceException, CompanyServiceException,
-			SearchServiceException, MessagingException;
+			SearchServiceException, MessagingException;*/
 
 	void getEmailsForUser(User usr) throws QuanthouseServiceException,
 			CompanyServiceException, SearchServiceException, MessagingException;
+	
+	 List<AlertOption> parseWatchlist(WatchlistModel watchlist, Account acct) 
+			 throws QuanthouseServiceException, CompanyServiceException, SearchServiceException;
 
 }
