051978dec86441d7c3901dde9f82313daa0896bf
scott <scott@c31ae540-8fef-4b06-900f-01f520cbaedc>
2015-12-02 16:39:36 +0000

    update to alerts. Added code to allow for only intgers and added length to input fields

--- trunk/clientv2/js/client/base.js
+++ trunk/clientv2/js/client/base.js
@@ -199,6 +199,32 @@ define(["jquery", "wmsi/page", "wmsi/utils", "knockout",  "text!client/data/glos
 		}
 	};
 	
+	// Input field to only allow numeric values
+	KO.bindingHandlers.numeric = {
+	    init: function (element) {
+	        $(element).on("keydown", function (event) {
+	            // Allow: backspace, delete, tab, escape, and enter
+	            if (event.keyCode == 46 || event.keyCode == 8 || event.keyCode == 9 || event.keyCode == 27 || event.keyCode == 13 ||
+	                // Allow: Ctrl+A
+	                (event.keyCode == 65 && event.ctrlKey === true) ||
+	                // Allow: . ,
+	                (event.keyCode == 188 || event.keyCode == 190 || event.keyCode == 110) ||
+	                // Allow: home, end, left, right
+	                (event.keyCode >= 35 && event.keyCode <= 39)) {
+	                // let it happen, don't do anything
+	                return;
+	            }
+	            else {
+	                // Ensure that it is a number and stop the keypress
+	                if (event.shiftKey || (event.keyCode < 48 || event.keyCode > 57) && (event.keyCode < 96 || event.keyCode > 105)) {
+	                    event.preventDefault();
+	                }
+	            }
+	        });
+	    }    
+	};
+	
+	
 	PAGE = {
 		fqdn: "",
 		//fqdn: "https://sgx-api-us.sharefc.com",
