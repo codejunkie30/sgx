2c5c9702a13c7949925d75e48f14bad2a32e8917
michael_parcewski <michael_parcewski@c31ae540-8fef-4b06-900f-01f520cbaedc>
2015-12-02 02:23:26 +0000

    null safe check

--- service/src/main/java/com/wmsi/sgx/domain/SortAccountByExpirationDateComparator.java
+++ service/src/main/java/com/wmsi/sgx/domain/SortAccountByExpirationDateComparator.java
@@ -2,11 +2,13 @@ package com.wmsi.sgx.domain;
 
 import java.util.Comparator;
 
+import org.apache.commons.lang.ObjectUtils;
+
 public class SortAccountByExpirationDateComparator implements Comparator<Account>
 {
 	@Override
 	public int compare(Account o1, Account o2) {
-		return o2.getExpirationDate().compareTo(o1.getExpirationDate());
+		return ObjectUtils.compare(o2.getExpirationDate(), o1.getExpirationDate());
 	}
 
 }
