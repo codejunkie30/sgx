3b9829a61b86d5bda3927ac3d20f52d4f8a96830
michael_parcewski <michael_parcewski@c31ae540-8fef-4b06-900f-01f520cbaedc>
2015-12-02 17:14:35 +0000

    custom generator for Karan

---
+++ service/src/main/java/com/wmsi/sgx/generator/IDGenerator.java
@@ -0,0 +1,54 @@
+package com.wmsi.sgx.generator;
+
+import java.io.Serializable;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.Properties;
+
+import org.hibernate.HibernateException;
+import org.hibernate.MappingException;
+import org.hibernate.dialect.Dialect;
+import org.hibernate.engine.spi.SessionImplementor;
+import org.hibernate.id.Configurable;
+import org.hibernate.id.IdentifierGenerator;
+import org.hibernate.id.PersistentIdentifierGenerator;
+import org.hibernate.type.Type;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class IDGenerator implements IdentifierGenerator, Configurable {
+
+	private static final Logger log = LoggerFactory.getLogger(IDGenerator.class);
+
+	private String query;
+
+	@Override
+	public void configure(Type type, Properties params, Dialect d) throws MappingException {
+		 String tableName = params.getProperty(PersistentIdentifierGenerator.TABLE);
+		 query = "SELECT NEXT VALUE FOR " + tableName + "_seq";
+	}
+	
+    /*
+     * (non-Javadoc)
+     * @see org.hibernate.id.IdentifierGenerator
+            #generate(org.hibernate.engine.SessionImplementor, java.lang.Object)
+     */
+    @Override
+    public Serializable generate(SessionImplementor session, Object object) throws HibernateException {
+    	
+    	try {
+    	
+    		final PreparedStatement pstmt = session.connection().prepareStatement(query);
+    		ResultSet rs = pstmt.executeQuery();
+    		while (rs.next()) return Long.parseLong(rs.getString(1));
+    		
+    	}
+    	catch(Exception e) {
+    		log.error("Trying to generate nextval", e);
+    	}
+
+        return null;
+    }
+    
+    
+}
