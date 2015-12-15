bb440439b9195c3eb67aba75048467e3da2b7cf1
dwfwu <dwfwu@c31ae540-8fef-4b06-900f-01f520cbaedc>
2015-12-15 21:47:17 +0000

    admin excel data changes

--- service/src/main/java/com/wmsi/sgx/repository/UserLoginRepository.java
+++ service/src/main/java/com/wmsi/sgx/repository/UserLoginRepository.java
@@ -4,6 +4,9 @@ import java.io.Serializable;
 import java.util.Date;
 import java.util.List;
 
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 import com.wmsi.sgx.domain.UserLogin;
 
 public interface UserLoginRepository extends CustomRepository<UserLogin, Serializable>{
@@ -12,4 +15,6 @@ public interface UserLoginRepository extends CustomRepository<UserLogin, Seriali
 	
 	List<UserLogin> findByUsernameAndDateGreaterThanOrderByDateDesc(String username, Date date);
 	
+	@Query("from UserLogin where username = :username order by date desc")
+	List<UserLogin> findByUsername(@Param("username")String username);
 }
