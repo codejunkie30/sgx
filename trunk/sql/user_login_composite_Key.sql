2fbeae15c5f7e1fec8349af040478fab8b5e63e1
ksethi <ksethi@c31ae540-8fef-4b06-900f-01f520cbaedc>
2016-03-01 21:31:38 +0000

    deleted duplicate copies

--- trunk/sql/user_login_composite_Key.sql
+++
@@ -1,9 +0,0 @@
-USE [sgx]
-GO
-
-ALTER TABLE [sgx].[dbo].[user_login] 
-DROP CONSTRAINT [PK_user_login]
-GO
-
-ALTER TABLE [sgx].[dbo].[user_login]
-ADD CONSTRAINT [PK_user_login] PRIMARY KEY (id,username,date)
