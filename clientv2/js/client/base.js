b775c572c60e2ae5c984015e9b0020f8d5c1812a
scott <scott@c31ae540-8fef-4b06-900f-01f520cbaedc>
2016-03-28 17:35:07 +0000

    Update to currency spacing

--- clientv2/js/client/base.js
+++ clientv2/js/client/base.js
@@ -385,14 +385,14 @@ define(["jquery", "wmsi/page", "wmsi/utils", "knockout",  "text!client/data/glos
 		"numberFormats-usd": {	
         	lookup: { header: "" },
         	string: { header: "" },
-			millions: { header: "in US$ mm", decimals: 1, format: "US$$VALUE mm" },
+			millions: { header: "in US$ mm", decimals: 1, format: "US$ $VALUE mm" },
 			volume: { header: "in mm", decimals: 2, format: "$VALUE mm" },
-			dollars: { header: "in US$", decimals: 3, format: "US$$VALUE" },
-			cents: { header: "in US$", decimals: 3, format: "US$$VALUE" },
+			dollars: { header: "in US$", decimals: 3, format: "US$ $VALUE" },
+			cents: { header: "in US$", decimals: 3, format: "US$ $VALUE" },
 			percent: { header: "in %", decimals:2, format: "$VALUE%" },
         	number: { header: "", decimals: 3 },
         	number1: { header: "", decimals: 1 },
-			chart: {format: "US$"}
+			chart: {format: "US$ "}
 		},		
         "numberFormats-sgd": {
         	lookup: { header: "" },
