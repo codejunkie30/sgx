a31053f6f0e488ec2d31fb090652122fffe550e3
ajay <ajay@c31ae540-8fef-4b06-900f-01f520cbaedc>
2016-03-31 19:00:56 +0000

    did minor change to Alert

--- trunk/clientv2/js/client/alerts.js
+++ trunk/clientv2/js/client/alerts.js
@@ -236,7 +236,7 @@ define([ "wmsi/utils", "knockout", "knockout-validate", "text!client/data/messag
 			
 			var newWLNameLC = ALERTS.newWLName();
 			
-			if (newWLNameLC.trim()==="" ) {  alert("Watchlist name is empty."); return; }
+			if (newWLNameLC.trim()==="" ) {  PAGE.modal.open({ type: 'alert',  content: '<p>Watchlist name is empty.</p>', width: 600 }); return; }
 			if ($.inArray( newWLNameLC.toLowerCase().trim(), ALERTS.addWatchlistName() ) != -1) {  PAGE.modal.open({ type: 'alert',  content: '<p>Watch list name already exists.</p>', width: 600 }); return; }
 			
 			if (wlLength >= 10) { PAGE.modal.open({ type: 'alert',  content: '<p>You can create up to 10 Watch Lists.</p>', width: 600 }); return; }
@@ -320,8 +320,8 @@ define([ "wmsi/utils", "knockout", "knockout-validate", "text!client/data/messag
 			});		
 			
 			var editedName = ALERTS.editWLName().trim();
-			if (editedName ==="" ) {  alert("Watchlist name is empty."); return; }
-			if ($.inArray( editedName.toLowerCase(), ALERTS.addWatchlistName() ) != -1) {  PAGE.modal.open({ type: 'alert',  content: '<p>Watch list name already exists.</p>', width: 600 }); return; }
+			if (editedName ==="" ) {  PAGE.modal.open({ type: 'alert',  content: '<p>Watchlist name is empty.</p>', width: 600 }); return; }
+			if ($.inArray( editedName.toLowerCase(), ALERTS.addWatchlistName() ) != -1) { PAGE.modal.open({ type: 'alert',  content: '<p>Watch list name already exists.</p>', width: 600 }); return;  }
 			
 			
     		var params = { "watchlistName": ALERTS.editWLName(), "id": ALERTS.selectedValue()};
