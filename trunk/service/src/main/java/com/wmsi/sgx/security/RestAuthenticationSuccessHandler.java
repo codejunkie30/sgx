629265c9511344a1c4f6d47bb442b33593ce871e
ksethi <ksethi@c31ae540-8fef-4b06-900f-01f520cbaedc>
2016-04-01 02:46:21 +0000

    Returning Account details with the login response

--- trunk/service/src/main/java/com/wmsi/sgx/security/RestAuthenticationSuccessHandler.java
+++ trunk/service/src/main/java/com/wmsi/sgx/security/RestAuthenticationSuccessHandler.java
@@ -14,7 +14,13 @@ import org.springframework.stereotype.Component;
 import com.fasterxml.jackson.core.JsonGenerationException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.wmsi.sgx.domain.User;
 import com.wmsi.sgx.domain.UserLogin;
+import com.wmsi.sgx.model.AccountModelWithAuthToken;
+import com.wmsi.sgx.model.account.AccountModel;
+import com.wmsi.sgx.security.token.TokenAuthenticationService;
+import com.wmsi.sgx.security.token.TokenHandler;
+import com.wmsi.sgx.service.account.AccountService;
 import com.wmsi.sgx.service.account.UserService;
 
 @Component
@@ -25,12 +31,26 @@ public class RestAuthenticationSuccessHandler extends SimpleUrlAuthenticationSuc
 	
 	@Autowired
 	private UserService userService;
+	
+	@Autowired
+	private AccountService accountService;
+	
+	@Autowired
+	private TokenAuthenticationService tokenAuthenticationService;
 		
 	@Override
 	protected void handle(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws JsonGenerationException, JsonMappingException, IOException{		
-		objectMapper.writeValue(response.getOutputStream(), response.getHeader("X-AUTH-TOKEN"));
 		
 		
+		AccountModel acc = accountService.getAccountForUsername(findUserFromToken(response.getHeader("X-AUTH-TOKEN")).getUsername());
+		AccountModelWithAuthToken res = new AccountModelWithAuthToken();
+		res.setEmail(acc.getEmail());
+		res.setCurrency(acc.getCurrency());
+		res.setContactOptIn(acc.getContactOptIn());
+		res.setType(acc.getType());
+		res.setToken(response.getHeader("X-AUTH-TOKEN"));
+		
+		objectMapper.writeValue(response.getOutputStream(), res);
 		WebAuthenticationDetails auth = (WebAuthenticationDetails) authentication.getDetails();
 		if(request.getServletPath().equals("/login")){
 			UserLogin login = new UserLogin();
@@ -38,9 +58,18 @@ public class RestAuthenticationSuccessHandler extends SimpleUrlAuthenticationSuc
 			login.setIpAddress(auth.getRemoteAddress());
 			login.setSuccess(true);
 			userService.recordLogin(login);
-
 		}
 		return;
+	}
+	
+	public User findUserFromToken(String token){
+		
+		
+		TokenHandler tokenHandler = tokenAuthenticationService.getTokenHandler();
+		User user = null;
+		if(token != null)
+		 return user = tokenHandler.parseUserFromToken(token);
+		return null;
 	}
 	
 }
