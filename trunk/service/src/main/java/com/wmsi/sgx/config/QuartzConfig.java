667470f0e401006ff3f9d1ee02d71e470ef57e3e
ksethi <ksethi@c31ae540-8fef-4b06-900f-01f520cbaedc>
2016-04-06 01:19:18 +0000

    adding quartz additonal config properties to stop multiple emails to fire

--- trunk/service/src/main/java/com/wmsi/sgx/config/QuartzConfig.java
+++ trunk/service/src/main/java/com/wmsi/sgx/config/QuartzConfig.java
@@ -133,6 +133,9 @@ public class QuartzConfig {
 		quartzProperties.put("org.quartz.jobStore.driverDelegateClass", "org.quartz.impl.jdbcjobstore.MSSQLDelegate");
 		quartzProperties.put("org.quartz.jobStore.isClustered", "true");
 		quartzProperties.put("org.quartz.jobStore.tablePrefix", "QRTZ_");
+		quartzProperties.put("org.quartz.jobStore.clusterCheckinInterval" ,"9000000");
+		quartzProperties.put("org.quartz.jobStore.misfireThreshold", "9000000");
+		
 		
 		
 		return quartzProperties;
