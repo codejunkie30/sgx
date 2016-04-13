e010ca70ab77f250c9614932161728279c9a03fc
scott <scott@c31ae540-8fef-4b06-900f-01f520cbaedc>
2016-04-13 21:12:25 +0000

    Update for GA code

--- clientv2/js/wmsi/page.js
+++ clientv2/js/wmsi/page.js
@@ -16,7 +16,7 @@ define(["jquery", "wmsi/utils", "wmsi/XD", ], function($, UTIL) {
 		 */
 		initPage: function() {
 			// initialize analytics
-			(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o), m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m) })(window,document,'script','//www.google-analytics.com/analytics.js','_gaTracker'); _gaTracker('create', this.gaClientId, { 'cookieDomain': 'none' });
+			(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o), m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m) })(window,document,'script','//www.google-analytics.com/analytics.js','_gaTracker'); _gaTracker('create', PAGE.gaClientId, { 'cookieDomain': 'none' });
 
 			// set the default page height
 			this.pageHeight = $(document).height();
