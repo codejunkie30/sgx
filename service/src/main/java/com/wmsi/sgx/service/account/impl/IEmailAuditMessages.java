3ecff47ca8b8cfb320ad684533819ebdb9670fc4
nprabha <nprabha@c31ae540-8fef-4b06-900f-01f520cbaedc>
2016-05-03 11:22:20 +0000

    899:Fix for price change

---
+++ service/src/main/java/com/wmsi/sgx/service/account/impl/IEmailAuditMessages.java
@@ -0,0 +1,14 @@
+package com.wmsi.sgx.service.account.impl;
+
+public class IEmailAuditMessages {
+
+	public static final String EMAIL_SUBJECT = "SGX StockFacts Premium Alert";
+
+	public static final String EMAIL_SUCCESS = "Success";
+
+	public static final String EMAIL_FAILED = "Failed";
+
+	public static final String WATCHLIST_UNAVAILABLE = "Watchlist doesn't contain companies or alert options";
+
+	public static final String NO_UPDATE_AVAILABLE = "No updates to watchlist companies. ";
+}
