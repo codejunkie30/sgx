75af9a9f2feb45def50da381122851bf06e714d1
aamoros <aamoros@c31ae540-8fef-4b06-900f-01f520cbaedc>
2016-05-05 18:31:26 +0000

    Assembla ticket #933

---
+++ trunk/xf-export/XFDataDump/Wmsi.SGX.Operations/Classes/GlobalConfig.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wmsi.SGX.Operations.Classes
+{
+    public static class GlobalConfig
+    {
+        public static string SGXDomain
+        {
+            get {
+                return ConfigurationManager.AppSettings["SGXDomain"];
+            }
+        }
+
+        public static string TickerURL
+        {
+            get
+            {
+                return ConfigurationManager.AppSettings["tickerURL"];
+            }
+        }
+
+        public static string XtraTickerURL
+        {
+            get
+            {
+                return ConfigurationManager.AppSettings["xtraTickerURL"];
+            }
+        }
+
+        public static string SGXValueVolumeURL
+        {
+            get
+            {
+                return ConfigurationManager.AppSettings["SGX_ValueVolume_URL"];
+            }
+        }
+
+        public static string SmtpHost
+        {
+            get
+            {
+                return ConfigurationManager.AppSettings["SmtpHost"];
+            }
+        }
+        public static int SmtpPort
+        {
+            get
+            {
+                return int.Parse(ConfigurationManager.AppSettings["SmtpPort"]);
+            }
+        }
+        public static string EmailTo
+        {
+            get
+            {
+                return ConfigurationManager.AppSettings["EmailTo"];
+            }
+        }
+        public static string EmailFrom
+        {
+            get
+            {
+                return ConfigurationManager.AppSettings["EmailFrom"];
+            }
+        }
+        public static string SmtpUserName
+        {
+            get
+            {
+                return ConfigurationManager.AppSettings["SmtpUserName"];
+            }
+        }
+        public static string SmtpPassword
+        {
+            get
+            {
+                return ConfigurationManager.AppSettings["SmtpPassword"];
+            }
+        }
+    }
+}
