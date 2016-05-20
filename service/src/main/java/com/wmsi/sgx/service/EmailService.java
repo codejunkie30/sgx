c57cf54bd11125c3f1e70e50f30be705cbd6e36a
dt84327 <dt84327@c31ae540-8fef-4b06-900f-01f520cbaedc>
2016-05-20 03:10:04 +0000

    Validation for the existence of keys and the generation of keys is a manual process now. If the keys are not exists then an email will be sent to sgx support

--- service/src/main/java/com/wmsi/sgx/service/EmailService.java
+++ service/src/main/java/com/wmsi/sgx/service/EmailService.java
@@ -5,5 +5,6 @@ import javax.mail.MessagingException;
 public interface EmailService{
 
 	void send(String to, String subject, String body, String resetemailbody) throws MessagingException;
+	void send(String to, String subject, String body) throws MessagingException;
 
 }
