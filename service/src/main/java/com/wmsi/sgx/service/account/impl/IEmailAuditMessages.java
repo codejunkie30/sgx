b8ac5fc460f00b18ac8235df736ba6d71ba5e8c1
nprabha <nprabha@c31ae540-8fef-4b06-900f-01f520cbaedc>
2016-05-31 10:33:29 +0000

    Added logging statements

--- service/src/main/java/com/wmsi/sgx/service/account/impl/IEmailAuditMessages.java
+++ service/src/main/java/com/wmsi/sgx/service/account/impl/IEmailAuditMessages.java
@@ -4,13 +4,13 @@ public class IEmailAuditMessages {
 
 	public static final String EMAIL_SUBJECT = "SGX StockFacts Premium Alert";
 
-	public static final String EMAIL_SUCCESS = "Success";
+	public static final String EMAIL_SUCCESS = "1.4 Success";
 
-	public static final String EMAIL_FAILED = "Failed";
+	public static final String EMAIL_FAILED = "1.4 Failed";
 
-	public static final String WATCHLIST_UNAVAILABLE = "Watchlist doesn't contain companies or alert options";
+	public static final String WATCHLIST_UNAVAILABLE = "1.4 Watchlist doesn't contain companies or alert options";
 
-	public static final String NO_UPDATE_AVAILABLE = "No updates to watchlist companies.";
+	public static final String NO_UPDATE_AVAILABLE = "1.4 No updates to watchlist companies.";
 	
-	public static final String UNKNOWN = "Unknown error happens";
+	public static final String UNKNOWN = "1.4 Unknown error happens";
 }
