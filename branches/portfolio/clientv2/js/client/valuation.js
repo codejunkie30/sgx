1deabf0e9ac911f6fc81251f8c6ecaa207b1380d
subrat <subrat@c31ae540-8fef-4b06-900f-01f520cbaedc>
2016-07-07 11:27:00 +0000

    Portfolio UI changes

--- branches/portfolio/clientv2/js/client/valuation.js
+++ branches/portfolio/clientv2/js/client/valuation.js
@@ -471,8 +471,8 @@ define([ "wmsi/utils", "knockout", "knockout-validate", "text!client/data/messag
 			});	
 			
 			if (me.wlNameError().length != 0) return;
-			if (newWLNameLC.trim()==="" ) {  PAGE.modal.open({ type: 'alert',  content: '<p>Watchlist name is empty.</p>', width: 600 }); return; }
-			if ($.inArray( newWLNameLC.toLowerCase().trim(), VALUATION.addWatchlistName() ) != -1) {  PAGE.modal.open({ type: 'alert',  content: '<p>Watch list name already exists.</p>', width: 600 }); return; }
+			if (newWLNameLC.trim()==="" ) {  PAGE.modal.open({ type: 'alert',  content: '<p>StockList name is empty.</p>', width: 600 }); return; }
+			if ($.inArray( newWLNameLC.toLowerCase().trim(), VALUATION.addWatchlistName() ) != -1) {  PAGE.modal.open({ type: 'alert',  content: '<p>StockList name already exists.</p>', width: 600 }); return; }
 			if (wlLength >= 10) { PAGE.modal.open({ type: 'alert',  content: '<p>You can create up to 10 StockLists.</p>', width: 600 }); return; }
 					
 			
