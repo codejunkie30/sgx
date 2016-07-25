39dee261ca6cc9e17fc3a2437e9047a4955b30ce
nprabha <nprabha@c31ae540-8fef-4b06-900f-01f520cbaedc>
2016-07-25 13:08:07 +0000

    898 Changes : Dynamica loading of currencies.

--- service/src/main/java/com/wmsi/sgx/controller/DataController.java
+++ service/src/main/java/com/wmsi/sgx/controller/DataController.java
@@ -1,5 +1,8 @@
 package com.wmsi.sgx.controller;
 
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,7 +12,11 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.wmsi.sgx.model.CurrencyModel;
+import com.wmsi.sgx.repository.Currencies;
 import com.wmsi.sgx.service.UtilService;
+import com.wmsi.sgx.service.currency.CurrencyService;
 
 @RestController
 @RequestMapping(method=RequestMethod.POST, produces="application/json")
@@ -20,7 +27,13 @@ public class DataController {
 	
 	@Value("${currencies.file.location}")
 	private String fileLocation;
-
+	
+	@Autowired
+	private Currencies currencyResponsitory;
+	
+	@Autowired
+	private CurrencyService currencySvc;
+	
 	@RequestMapping("currencyList")
 	public String getCurrencyList() {
 		Map<Object, Object> map = utilService.convertCurrencyCSVtoMap(fileLocation);
@@ -29,4 +42,23 @@ public class DataController {
 		Gson gson = new Gson(); 
 		return gson.toJson(map); 
 	}
+	
+	private void createCurrencyCSVFile(){
+		Gson gson = new Gson();	
+		List<CurrencyModel> currencyList = currencySvc.getAllCurrencies();
+		java.lang.reflect.Type typeOfSrc = new TypeToken<Collection<CurrencyModel>>(){}.getType();
+		gson.toJson(currencyList, typeOfSrc);
+	}
+	
+	private Map<String,String> getCurrencyMap(){
+		Gson gson = new Gson();	
+		List<CurrencyModel> currencyList = currencySvc.getAllCurrencies();
+		Map<String,String>currencyMap = new HashMap<String,String>();
+		for(CurrencyModel m : currencyList){
+			currencyMap.put(m.getCurrencyName(), m.getDescription());
+		}
+		return currencyMap;
+	}
+	
+
 }
