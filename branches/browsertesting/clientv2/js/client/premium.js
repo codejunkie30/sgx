67ce4b3c0796d4b6ae77cda7eb0bf89924f84b93
dt84327 <dt84327@c31ae540-8fef-4b06-900f-01f520cbaedc>
2016-08-16 04:45:21 +0000

    1104 - Changes to display sign in bar "Learn More" Page

--- branches/browsertesting/clientv2/js/client/premium.js
+++ branches/browsertesting/clientv2/js/client/premium.js
@@ -4,7 +4,18 @@ define([ "wmsi/utils", "knockout", "text!client/data/messages.json" ], function(
 		messages: JSON.parse(MESSAGES),
 		price: ko.observable(),
 		content: ko.observable(),
+		premiumUser: ko.observable(),
+		premiumUserEmail: ko.observable(),		
+		premiumUserAccntInfo: ko.observable(),
+		libLoggedIn: ko.observable(),
+		libTrialPeriod: ko.observable(),
+		libTrialExpired: ko.observable(),
+		libSubscribe: ko.observable(),
+		libAlerts: ko.observable(),
+		libCurrency: ko.observable(false),
+		currentDay: ko.observable(),
 		initPage: function() {
+			PAGE.checkStatus();
 			var displayMessage = PREMIUM.messages.messages[0];
 			
 			PREMIUM.price(displayMessage.premium.price);
