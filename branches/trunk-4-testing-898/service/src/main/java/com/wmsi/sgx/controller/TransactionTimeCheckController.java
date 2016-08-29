3b552a063dd21dc663f1117fb2934b2250fe0263
nprabha <nprabha@c31ae540-8fef-4b06-900f-01f520cbaedc>
2016-08-29 11:41:20 +0000

    Refactored the token generation Reusing the tokenAuthenticationService - TokenAuthenticationSErvice is the GOTO point Included DeleteToken to the repository Added new Service for renewing the token

--- branches/trunk-4-testing-898/service/src/main/java/com/wmsi/sgx/controller/TransactionTimeCheckController.java
+++ branches/trunk-4-testing-898/service/src/main/java/com/wmsi/sgx/controller/TransactionTimeCheckController.java
@@ -2,10 +2,7 @@ package com.wmsi.sgx.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
-import com.wmsi.sgx.security.token.TokenAuthenticationService;
-import com.wmsi.sgx.security.token.TokenHandler;
 import com.wmsi.sgx.security.token.TransactionTokenAuthenticationService;
-import com.wmsi.sgx.security.token.TransactionTokenHandler;
 
 public class TransactionTimeCheckController
 {
