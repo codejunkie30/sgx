e9d459ac0dd6bd86902b1b0f3387c01bab42134b
nprabha <nprabha@c31ae540-8fef-4b06-900f-01f520cbaedc>
2016-08-30 10:32:47 +0000

    Initial checkin for Renew Token

--- service/src/main/java/com/wmsi/sgx/security/RestLogoutSuccessHandler.java
+++ service/src/main/java/com/wmsi/sgx/security/RestLogoutSuccessHandler.java
@@ -8,7 +8,6 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
-import org.springframework.security.web.authentication.logout.SimpleUrlLogoutSuccessHandler;
 import org.springframework.stereotype.Component;
 
 @Component
