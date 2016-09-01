3ab51c495ce1d498317ecac8f3ac6702a4e0d93b
ajay <ajay@c31ae540-8fef-4b06-900f-01f520cbaedc>
2016-09-01 09:54:34 +0000

    Modified as part of LO6 and LO7 requirements.

--- branches/trunk-4-testing-898/service/src/main/java/com/wmsi/sgx/security/RestLogoutSuccessHandler.java
+++ branches/trunk-4-testing-898/service/src/main/java/com/wmsi/sgx/security/RestLogoutSuccessHandler.java
@@ -6,21 +6,36 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 import org.springframework.stereotype.Component;
 
+import com.wmsi.sgx.domain.User;
+import com.wmsi.sgx.security.token.TokenAuthenticationService;
+import com.wmsi.sgx.security.token.TokenHandler;
+
 @Component
 public class RestLogoutSuccessHandler implements LogoutSuccessHandler{
 
-	
+  @Autowired
+  private TokenAuthenticationService tokenAuthenticationService;
+  
 	@Override
 	public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication)
 			throws IOException, ServletException {
-		
+	  deleteUserTransactionTokens(request);
 		 response.setStatus(HttpServletResponse.SC_OK);
 	     response.getWriter().flush();
 		
+	}
+	
+	private void deleteUserTransactionTokens(HttpServletRequest request)
+	{
+	  String token = request.getHeader("X-AUTH-TOKEN");
+    TokenHandler tokenHandler = tokenAuthenticationService.getTokenHandler();
+    User user = tokenHandler.parseUserFromToken(token);
+    tokenAuthenticationService.clearAllTxSessionTokens( user );
 	}
 	
 }
