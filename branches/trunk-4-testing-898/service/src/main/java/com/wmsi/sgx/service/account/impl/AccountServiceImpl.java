4a76b71c97bad5e6778089c026df89c36de2e886
nprabha <nprabha@c31ae540-8fef-4b06-900f-01f520cbaedc>
2016-09-22 09:27:57 +0000

    #1116 :- Trial ,Premium users should be able to use all features on the all day of expiration.

--- branches/trunk-4-testing-898/service/src/main/java/com/wmsi/sgx/service/account/impl/AccountServiceImpl.java
+++ branches/trunk-4-testing-898/service/src/main/java/com/wmsi/sgx/service/account/impl/AccountServiceImpl.java
@@ -4,7 +4,6 @@ import java.util.Calendar;
 import java.util.Collections;
 import java.util.Date;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
 
 import org.joda.time.DateTime;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,196 +26,184 @@ import com.wmsi.sgx.service.search.elasticsearch.ElasticSearchService;
 import com.wmsi.sgx.util.DateUtil;
 
 @Service
-public class AccountServiceImpl implements AccountService{
+public class AccountServiceImpl implements AccountService {
 
 	@Autowired
 	private AccountRepository accountRepository;
-	
+
 	@Autowired
 	private EnetsRepository enetsRepository;
 
 	@Autowired
 	private TrialProperty getTrial;
-	
+
 	@Autowired
 	private ElasticSearchService esSearchService;
-	
+
 	private static final int PREMIUM_EXPIRATION_DAYS = 365;
-	
+
 	/*
-	 * TODO
-	 * Add constraint to account table for unique user, accountType, active?
-	 * Add unique constraint user/active
-	 * Add method to repo for findByUserTypeActive
+	 * TODO Add constraint to account table for unique user, accountType,
+	 * active? Add unique constraint user/active Add method to repo for
+	 * findByUserTypeActive
 	 */
-	
+
 	@Override
-	//@Secured("ROLE_USER")
-	public AccountModel getAccountForUsername(String username){
-		
+	// @Secured("ROLE_USER")
+	public AccountModel getAccountForUsername(String username) {
+
 		// TODO, ensure there is only ever one active account
-		List<Account> accounts = accountRepository.findByUsername(username);		
+		List<Account> accounts = accountRepository.findByUsername(username);
 		AccountModel ret = null;
 		Collections.sort(accounts, new SortAccountByExpirationDateComparator());
 		Account account;
-		if(accounts.size() > 0){
+		if (accounts.size() > 0) {
 			account = accounts.get(0);
 			ret = new AccountModel();
 			ret.setEmail(account.getUser().getUsername());
-			ret.setStartDate(resetToMidnightTime(account.getStartDate()));
-			if(account.getExpirationDate() != null){
-				ret.setExpirationDate(resetToMidnightTime(account.getExpirationDate()));
-				
-			}else{
-				Date expiration = DateUtil.toDate(DateUtil.adjustDate(DateUtil.fromDate(account.getStartDate()), Calendar.DAY_OF_MONTH, account.getType() == AccountType.TRIAL ? getTrial.getTrialDays() : PREMIUM_EXPIRATION_DAYS));
-				ret.setExpirationDate(expiration);
+			ret.setStartDate(DateUtil.resetToMidnightTime(account.getStartDate()));
+			if (account.getExpirationDate() != null) {
+				ret.setExpirationDate(DateUtil.resetToMidnightTime(account.getExpirationDate()));
+
+			} else {
+				Date expiration = DateUtil.toDate(DateUtil.adjustDate(DateUtil.fromDate(account.getStartDate()),
+						Calendar.DAY_OF_MONTH,
+						account.getType() == AccountType.TRIAL ? getTrial.getTrialDays() : PREMIUM_EXPIRATION_DAYS));
+				ret.setExpirationDate(DateUtil.resetToMidnightTime(expiration));
 			}
-			if(account.getActive().equals(false)){
+			if (account.getActive().equals(false)) {
 				ret.setType(AccountType.EXPIRED);
-			}else{
+			} else {
 				ret.setType(account.getType());
 			}
-			ret.setDaysRemaining(getDaysRemaining(ret));
+			ret.setDaysRemaining(DateUtil.getDaysRemaining(ret.getExpirationDate()));
 			ret.setContactOptIn(account.getContactOptIn());
 			ret.setCurrency(account.getCurrency());
-			if(account.getType() == AccountType.PREMIUM || account.getType() == AccountType.ADMIN 
-					|| account.getType() == AccountType.MASTER){
-				/*EnetsTransactionDetails enetsTrasactionDetail = enetsRepository.findByUserAndActive(account.getActive(), account.getUser().getId());
-				if(enetsTrasactionDetail != null){
-					ret.setEnetsTranId((enetsTrasactionDetail.getTrans_id()));
-				}*/
-				List<EnetsTransactionDetails> enetsTrasactionDetail = enetsRepository.findByUserAndActive(account.getActive(), account.getUser().getId());
-				if(enetsTrasactionDetail.size() != 0){
+			if (account.getType() == AccountType.PREMIUM || account.getType() == AccountType.ADMIN
+					|| account.getType() == AccountType.MASTER) {
+				/*
+				 * EnetsTransactionDetails enetsTrasactionDetail =
+				 * enetsRepository.findByUserAndActive(account.getActive(),
+				 * account.getUser().getId()); if(enetsTrasactionDetail !=
+				 * null){
+				 * ret.setEnetsTranId((enetsTrasactionDetail.getTrans_id())); }
+				 */
+				List<EnetsTransactionDetails> enetsTrasactionDetail = enetsRepository
+						.findByUserAndActive(account.getActive(), account.getUser().getId());
+				if (enetsTrasactionDetail.size() != 0) {
 					Collections.sort(enetsTrasactionDetail, new SortDatesDecendingEnetsTransactionId());
 					ret.setEnetsTranId((enetsTrasactionDetail.get(0).getTrans_id()));
 				}
-				
+
 			}
 		}
-			
-			return ret;
-	}
 
-	/**
-	 * @param ret
-	 */
-	private long getDaysRemaining(AccountModel ret) {
-		long diff = ret.getExpirationDate().getTime() - new Date().getTime();
-		return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
-	}
-	
-	private Date resetToMidnightTime(Date expirationDate) {
-		// TODO Auto-generated method stub
-		Calendar cal = Calendar.getInstance(); // locale-specific
-		cal.setTime(expirationDate);
-		cal.set(Calendar.HOUR_OF_DAY, 0);
-		cal.set(Calendar.MINUTE, 0);
-		cal.set(Calendar.SECOND, 0);
-		cal.set(Calendar.MILLISECOND, 0);
-		long time = cal.getTimeInMillis();
-		return cal.getTime();
+		return ret;
 	}
 
 	@Override
-	public User findUserForTransactionId(String transId){
+	public User findUserForTransactionId(String transId) {
 		return enetsRepository.findByTransactionId(transId);
 	}
-	
-	public Boolean setCurrencyIndex(String esIndexName){
+
+	public Boolean setCurrencyIndex(String esIndexName) {
 		esSearchService.setIndexName(esIndexName);
 		return false;
 	}
-	
+
 	@Override
-	//@Secured("ROLE_USER")
-	public AccountModel updateAccount(UpdateAccountModel dto, long updatedBy){
-		
-		List<Account> accounts = accountRepository.findByUsername(dto.getEmail());		
+	// @Secured("ROLE_USER")
+	public AccountModel updateAccount(UpdateAccountModel dto, long updatedBy) {
+
+		List<Account> accounts = accountRepository.findByUsername(dto.getEmail());
 		Collections.sort(accounts, new SortAccountByExpirationDateComparator());
-		if(accounts.size() > 0){
-		accounts.get(0).setContactOptIn(dto.getContactOptIn());
-		accounts.get(0).setCurrency(dto.getCurrency());
-		accountRepository.accountserviceUpdateAccount(accounts.get(0).getContactOptIn(), accounts.get(0).getCurrency(), accounts.get(0).getUser().getId(), updatedBy, new Date());
+		if (accounts.size() > 0) {
+			accounts.get(0).setContactOptIn(dto.getContactOptIn());
+			accounts.get(0).setCurrency(dto.getCurrency());
+			accountRepository.accountserviceUpdateAccount(accounts.get(0).getContactOptIn(),
+					accounts.get(0).getCurrency(), accounts.get(0).getUser().getId(), updatedBy, new Date());
 		}
 		return getAccountForUsername(dto.getEmail());
-		
+
 	}
-	
-	
+
 	@Override
-	public Account createTrialAccount(User user) throws AccountCreationException{
-	
+	public Account createTrialAccount(User user) throws AccountCreationException {
+
 		List<Account> accounts = accountRepository.findAllByUser(user);
-		
-		// Sanity check, there shouldn't be any other accounts yet. 
-		if(accounts.size() > 0)
+
+		// Sanity check, there shouldn't be any other accounts yet.
+		if (accounts.size() > 0)
 			throw new AccountCreationException();
-			
+
 		return createAccount(user, AccountType.TRIAL, getTrial.getTrialDays());
-		
+
 	}
-	
+
 	@Override
-	public Account createPremiumAccount(User user){
-		
+	public Account createPremiumAccount(User user) {
+
 		List<Account> accounts = accountRepository.findAllByUser(user);
-		
-		for(Account acc : accounts){
-			
+
+		for (Account acc : accounts) {
+
 			// Deactivate trial and Premium accounts
-			if(acc.getType().equals(AccountType.TRIAL) || acc.getType().equals(AccountType.PREMIUM)){
+			if (acc.getType().equals(AccountType.TRIAL) || acc.getType().equals(AccountType.PREMIUM)) {
 				acc.setActive(false);
-				accountRepository.updateActive(acc.getActive(), acc.getUser().getId(), acc.getUser().getId(), new Date());
+				accountRepository.updateActive(acc.getActive(), acc.getUser().getId(), acc.getUser().getId(),
+						new Date());
 			}
 		}
-		
+
 		return createAccount(user, AccountType.PREMIUM, PREMIUM_EXPIRATION_DAYS);
-		
+
 	}
-	
+
 	@Override
-	public Boolean isPremiumUser(User u){
-		if(u!=null){
-			AccountModel accountModel =getAccountForUsername(u.getUsername());	
-			if(accountModel.getType()== AccountType.PREMIUM || accountModel.getType()== AccountType.TRIAL 
+	public Boolean isPremiumUser(User u) {
+		if (u != null) {
+			AccountModel accountModel = getAccountForUsername(u.getUsername());
+			if (accountModel.getType() == AccountType.PREMIUM || accountModel.getType() == AccountType.TRIAL
 					|| accountModel.getType() == AccountType.ADMIN || accountModel.getType() == AccountType.MASTER)
-					return true;
+				return true;
 		}
 		return false;
 	}
-	
+
 	@Override
-	public Boolean convertToExpiry(User user){
+	public Boolean convertToExpiry(User user) {
 		Boolean success = false;
 		List<Account> accounts = accountRepository.findAllByUser(user);
-		
-		for(Account acc : accounts){
-			
+
+		for (Account acc : accounts) {
+
 			// Deactivate trial account
-			if(acc.getType().equals(AccountType.TRIAL) || (acc.getType().equals(AccountType.PREMIUM))){
+			if (acc.getType().equals(AccountType.TRIAL) || (acc.getType().equals(AccountType.PREMIUM))) {
 				acc.setActive(false);
-				accountRepository.updateActive(acc.getActive(), acc.getUser().getId(), acc.getUser().getId(), new Date());
-				success  = true;
+				accountRepository.updateActive(acc.getActive(), acc.getUser().getId(), acc.getUser().getId(),
+						new Date());
+				success = true;
 			}
 		}
-		
+
 		return success;
-		
+
 	}
-	private Account createAccount(User user, AccountType type, int expirationDays){
+
+	private Account createAccount(User user, AccountType type, int expirationDays) {
 
 		Account acc = new Account();
 		acc.setType(type);
 		acc.setUser(user);
-		acc.setStartDate(resetToMidnightTime(new Date()));
-		if(type == AccountType.TRIAL)
+		acc.setStartDate(DateUtil.resetToMidnightTime(new Date()));
+		if (type == AccountType.TRIAL)
 			acc.setExpirationDate(null);
 		else
-			acc.setExpirationDate(resetToMidnightTime(new DateTime().plusDays(expirationDays).toDate()));
+			acc.setExpirationDate(DateUtil.resetToMidnightTime(new DateTime().plusDays(expirationDays).toDate()));
 		acc.setActive(true);
 		acc.setAlwaysActive(false);
 		acc.setContactOptIn(user.getContactOptIn());
 		return accountRepository.save(acc);
-		
+
 	}
 }
