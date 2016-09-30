3ac2fbc3270984afeaebf49c48a6d81e147bcbee
dt84327 <dt84327@c31ae540-8fef-4b06-900f-01f520cbaedc>
2016-09-30 11:27:10 +0000

    1130  Changes for expiration date issue

--- service/src/main/java/com/wmsi/sgx/model/account/AdminResponse.java
+++ service/src/main/java/com/wmsi/sgx/model/account/AdminResponse.java
@@ -31,7 +31,6 @@ public class AdminResponse {
 	public Date getDateParam() {
 		return dateParam;
 	}
-	@JsonSerialize(using=JsonDateSerializer.class)
 	public void setDateParam(Date dateParam) {
 		this.dateParam = dateParam;
 	}
