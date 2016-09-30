67862a06f2d04c1b0c6f2158cecd619b7061f8eb
dt84327 <dt84327@c31ae540-8fef-4b06-900f-01f520cbaedc>
2016-09-30 09:38:46 +0000

    Merging SQL changes into trunk

---
+++ sql/20_add_audit_columns.sql
@@ -0,0 +1,17 @@
+ALTER TABLE watchlist_transaction
+    ADD created_by bigint NOT NULL DEFAULT 1,
+        created_dt datetime2(3) NOT NULL DEFAULT GETDATE(),
+        updated_by bigint NOT NULL DEFAULT 1,
+		updated_dt datetime2(3) NOT NULL DEFAULT GETDATE()
+
+ALTER TABLE configurations
+    ADD ChangedDt datetime2(3) NOT NULL DEFAULT GETDATE()
+    
+ALTER TABLE premium_verification
+    ADD updated_dt datetime2(3) NOT NULL DEFAULT GETDATE()
+    
+ALTER TABLE user_verification
+    ADD updated_dt datetime2(3) NOT NULL DEFAULT GETDATE()
+    
+ALTER TABLE watchlist
+    ADD updated_dt datetime2(3) NOT NULL DEFAULT GETDATE()
