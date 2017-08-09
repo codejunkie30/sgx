d4971b754ba4267216e6e6d9066acd7805a73f1f
ksethi <ksethi@c31ae540-8fef-4b06-900f-01f520cbaedc>
2017-08-09 17:19:21 +0000

    File is updated as requested by S&P. Ticket details: RSCC-12554

--- trunk/xf-export/queries/ownership.sql
+++ trunk/xf-export/queries/ownership.sql
@@ -1,21 +1,22 @@
 select
 	pop.tickerSymbol,
 	pop.exchangeSymbol,
-	case when companyName is null
-		then firstName+' '+lastName
-		else companyName end as name,
+case when companyName is null
+then firstName+' '+lastName
+else companyName end as name,
 	a.sharesHeld as shares,
 	a.percentOfSharesOutstanding as "percent"
 from (
 	select a.ownerObjectId, a.ownedCompanyId, a.sharesHeld, a.percentOfSharesOutstanding,rank()
-		over (Partition by ownedcompanyId order by a.sharesheld desc) as rank
-	from ciqOwnCompanyHolding a 
+	over (Partition by ownedcompanyId order by a.sharesheld desc) as rank
+		from ciqOwnCompanyHolding a 
 		join ciqOwnHoldingPeriod b on a.periodid=b.periodid
-	where periodEndDate > getdate()
+		where periodEndDate > getdate()
 		and a.ownedCompanyId in (select companyId from ##sgxpop)
+		and not exists (select NULL from ciqCompany cc where cc.companyID = a.ownerObjectID and cc.companyTypeID in (9,13) )
 	) a
-	left join ciqPerson per on a.ownerObjectId=per.personId
-	left join ciqCompany co on a.ownerObjectId=co.companyId
-	join ##sgxpop pop on a.ownedCompanyId=pop.companyId
+left join ciqPerson per on a.ownerObjectId=per.personId
+left join ciqCompany co on a.ownerObjectId=co.companyId
+join ##sgxpop pop on a.ownedCompanyId=pop.companyId
 where rank <=5
 order by ownedCompanyId, sharesHeld desc
